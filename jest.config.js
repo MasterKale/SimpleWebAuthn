db6f558e96f7f27ba353d8f4e6303a021f6a7302
Matthew Miller <matthew@millerti.me>
2020-05-19 01:02:37 -0700

    Add mappers to Jest config for absolute paths

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  'moduleNameMapper': {
+    '@helpers/(.*)': '<rootDir>/src/helpers/$1',
+    '@types': '<rootDir>/src/types',
+  },
 };
