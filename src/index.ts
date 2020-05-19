7bb996683e6ed0b87e4fd33fccf6e0c1d39ff99a
Matthew Miller <matthew@millerti.me>
2020-05-19 01:58:57 -0700

    Add attestation verification to lib export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import generateAttestationCredentials from './attestation/generateAttestationCredentials';
+import verifyAttestationResponse from './attestation/verifyAttestationResponse';
 import generateAssertionCredentials from './assertion/generateAssertionCredentials';
 
 export {
+  // Attestation (e.g. Registration)
   generateAttestationCredentials,
+  verifyAttestationResponse,
+  // Assertion (e.g. Login)
   generateAssertionCredentials,
 };
