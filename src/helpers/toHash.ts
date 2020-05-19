b9a6310ec7ff4f94501a2c6a72b6cf773ca66762
Matthew Miller <matthew@millerti.me>
2020-05-18 17:23:54 -0700

    Create toHash helper

---
+++ src/helpers/toHash.ts
@@ -0,0 +1,10 @@
+import crypto from 'crypto';
+
+/**
+ * Returns hash digest of the given data using the given algorithm.
+ * @param data Data to hash
+ * @return The hash
+ */
+export default function toHash(data: Buffer, algo: string = 'SHA256'): Buffer {
+  return crypto.createHash(algo).update(data).digest();
+}
