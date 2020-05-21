2b9998d38a81e70b94ee7fb0cfada0a6478d2808
Matthew Miller <matthew@millerti.me>
2020-05-21 08:40:59 -0700

    Add startAssertion to browser

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -1,7 +1,9 @@
 import startAttestation from './methods/startAttestation';
+import startAssertion from './methods/startAssertion';
 import supportsWebauthn from './helpers/supportsWebauthn';
 
 export {
   startAttestation,
+  startAssertion,
   supportsWebauthn,
 };
---
+++ packages/browser/src/methods/startAssertion.ts
@@ -0,0 +1,54 @@
+import {
+  PublicKeyCredentialRequestOptionsJSON,
+  AuthenticatorAssertionResponseJSON,
+  AssertionCredential,
+} from '@webauthntine/typescript-types';
+
+import toUint8Array from '../helpers/toUint8Array';
+import toBase64String from '../helpers/toBase64String';
+import supportsWebauthn from '../helpers/supportsWebauthn';
+
+/**
+ * Begin authenticator "login" via WebAuthn assertion
+ *
+ * @param requestOptionsJSON Output from @webauthntine/server's generateAssertionOptions(...)
+ */
+export default async function startAssertion(
+  requestOptionsJSON: PublicKeyCredentialRequestOptionsJSON
+): Promise<AuthenticatorAssertionResponseJSON> {
+  if (!supportsWebauthn()) {
+    throw new Error('Webauthn is not supported in this browser');
+  }
+
+  // We need to convert some values to Uint8Arrays before passing the credentials to the navigator
+  const publicKey: PublicKeyCredentialRequestOptions = {
+    ...requestOptionsJSON.publicKey,
+    challenge: toUint8Array(requestOptionsJSON.publicKey.challenge),
+    allowCredentials: requestOptionsJSON.publicKey.allowCredentials.map((cred) => ({
+      ...cred,
+      id: toUint8Array(cred.id),
+    }))
+  };
+
+  // Wait for the user to complete assertion
+  const credential = await navigator.credentials.get({ publicKey });
+
+  if (!credential) {
+    throw new Error('Assertion was not completed');
+  }
+
+  const { response } = (credential as AssertionCredential);
+
+  let base64UserHandle = undefined;
+  if (response.userHandle) {
+    base64UserHandle = toBase64String(response.userHandle);
+  }
+
+  // Convert values to base64 to make it easier to send back to the server
+  return {
+    base64AuthenticatorData: toBase64String(response.authenticatorData),
+    base64ClientDataJSON: toBase64String(response.clientDataJSON),
+    base64Signature: toBase64String(response.signature),
+    base64UserHandle,
+  };
+}
