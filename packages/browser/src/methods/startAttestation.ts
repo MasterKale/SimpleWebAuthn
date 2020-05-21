e1a5fc6bd77be5cc6a67c51520209cf8639bca40
Matthew Miller <matthew@millerti.me>
2020-05-20 23:55:24 -0700

    Add more docs to startAttestation in browser

--- packages/browser/src/methods/startAttestation.ts
+++ packages/browser/src/methods/startAttestation.ts
@@ -9,7 +9,9 @@ import toBase64String from '../helpers/toBase64String';
 import supportsWebauthn from '../helpers/supportsWebauthn';
 
 /**
+ * Begin authenticator registration via WebAuthn "attestation"
  *
+ * @param creationOptionsJSON Output from @webauthntine/server's generateAttestationOptions(...)
  */
 export default async function startAttestation(
   creationOptionsJSON: PublicKeyCredentialCreationOptionsJSON
