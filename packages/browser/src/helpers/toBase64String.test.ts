8a464cdfdd9bb707aa0525b414c00342f58fd94d
Matthew Miller <matthew@millerti.me>
2020-05-22 15:23:29 -0700

    Fix misspelling in toBase64String test

--- packages/browser/src/helpers/toBase64String.test.ts
+++ packages/browser/src/helpers/toBase64String.test.ts
@@ -1,6 +1,6 @@
 import toBase64String from './toBase64String';
 
-import toUintArray from './toUint8Array';
+import toUint8Array from './toUint8Array';
 
 test('should convert a Buffer to a string with a length that is a multiple of 4', () => {
   const base64 = toBase64String(Buffer.from('123456', 'ascii'));
@@ -9,7 +9,7 @@ test('should convert a Buffer to a string with a length that is a multiple of 4'
 });
 
 test('should convert a Uint8Array to a string with a length that is a multiple of 4', () => {
-  const base64 = toBase64String(toUintArray('123456'));
+  const base64 = toBase64String(toUint8Array('123456'));
 
   expect(base64.length % 4).toEqual(0);
 });
