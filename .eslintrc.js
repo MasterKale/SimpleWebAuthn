0b7223af636ff254b41de385bfd15c7fbbb5cd95
Matthew Miller <matthew@millerti.me>
2020-05-25 11:35:42 -0700

    Set up ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
