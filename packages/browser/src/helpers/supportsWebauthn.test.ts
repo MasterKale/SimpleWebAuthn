ab04d0e332174d1df80af0446c955dc1abb56c6c
Pascal Sthamer <sthamer.pascal@gmail.com>
2020-05-25 19:22:46 +0200

    Add test for window being undefined

--- packages/browser/src/helpers/supportsWebauthn.test.ts
+++ packages/browser/src/helpers/supportsWebauthn.test.ts
@@ -1,4 +1,4 @@
-import supportsWebauthn from './supportsWebauthn';
+import supportsWebauthn from './supportsWebauthn'
 
 beforeEach(() => {
   // @ts-ignore 2741
@@ -13,3 +13,16 @@ test('should return false when browser does not support WebAuthn', () => {
   delete window.PublicKeyCredential;
   expect(supportsWebauthn()).toBe(false);
 });
+
+test('should return false when window is undefined', () => {
+  // Make window undefined as it is in node environments.
+  // @ts-expect-error
+  const windowSpy = jest.spyOn(global, "window", "get");
+  windowSpy.mockImplementation(() => undefined);
+
+  expect(window).toBe(undefined)
+  expect(supportsWebauthn()).toBe(false);
+
+  // Restore original window value.
+  windowSpy.mockRestore()
+})
