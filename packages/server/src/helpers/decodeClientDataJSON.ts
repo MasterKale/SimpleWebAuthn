328dc36408c2d3d1eac4829bd4cee6a6b573af99
Matthew Miller <matthew@millerti.me>
2020-06-05 17:56:47 -0700

    Use base64url to decode clientDataJSON

--- packages/server/src/helpers/decodeClientDataJSON.ts
+++ packages/server/src/helpers/decodeClientDataJSON.ts
@@ -7,9 +7,9 @@ export default function decodeClientDataJSON(data: string): ClientDataJSON {
   const toString = base64url.decode(data);
   const clientData: ClientDataJSON = JSON.parse(toString);
 
-  // `challenge` will be Base64URL-encoded here. Decode it for easier comparisons with what is provided
-  // as the expected value
-  clientData.challenge = Buffer.from(clientData.challenge, 'base64').toString('ascii');
+  // `challenge` will be Base64URL-encoded here. Decode it for easier comparisons with what is
+  // provided as the expected value
+  clientData.challenge = base64url.decode(clientData.challenge);
 
   return clientData;
 }
