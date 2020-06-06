506d1a5beb91ad5e65b30e10cb056d74bc49298c
Matthew Miller <matthew@millerti.me>
2020-06-05 17:55:58 -0700

    Replace “base64” with “base64url” in some comments

--- packages/server/src/helpers/decodeClientDataJSON.ts
+++ packages/server/src/helpers/decodeClientDataJSON.ts
@@ -1,13 +1,13 @@
 import base64url from 'base64url';
 
 /**
- * Decode an authenticator's base64-encoded clientDataJSON to JSON
+ * Decode an authenticator's base64url-encoded clientDataJSON to JSON
  */
 export default function decodeClientDataJSON(data: string): ClientDataJSON {
   const toString = base64url.decode(data);
   const clientData: ClientDataJSON = JSON.parse(toString);
 
-  // `challenge` will be Base64-encoded here. Decode it for easier comparisons with what is provided
+  // `challenge` will be Base64URL-encoded here. Decode it for easier comparisons with what is provided
   // as the expected value
   clientData.challenge = Buffer.from(clientData.challenge, 'base64').toString('ascii');
 
