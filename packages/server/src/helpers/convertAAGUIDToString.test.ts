c46154bd28805c4c597c7aa49e975c220a43a02a
Matthew Miller <matthew@millerti.me>
2020-06-20 12:24:50 -0700

    Add helper for converting aaguid to UUID string

---
+++ packages/server/src/helpers/convertAAGUIDToString.test.ts
@@ -0,0 +1,7 @@
+import convertAAGUIDToString from './convertAAGUIDToString';
+
+test('should convert buffer to UUID string', () => {
+  const uuid = convertAAGUIDToString(Buffer.from('adce000235bcc60a648b0b25f1f05503', 'hex'));
+
+  expect(uuid).toEqual('adce0002-35bc-c60a-648b-0b25f1f05503');
+});
