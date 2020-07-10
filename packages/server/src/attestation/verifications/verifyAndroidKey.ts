15524de87f1e80ec96a4eb987b2e642c4e0ec21c
Matthew Miller <matthew@millerti.me>
2020-07-10 08:06:07 -0700

    Improve ASN.1 parsing in verifyAndroidKey

--- packages/server/src/attestation/verifications/verifyAndroidKey.ts
+++ packages/server/src/attestation/verifications/verifyAndroidKey.ts
@@ -1,7 +1,9 @@
+import { AsnParser } from '@peculiar/asn1-schema';
+import { KeyDescription, id_ce_keyDescription } from '@peculiar/asn1-android';
+
 import type { AttestationStatement } from '../../helpers/decodeAttestationObject';
 import convertASN1toPEM from '../../helpers/convertASN1toPEM';
 import verifySignature from '../../helpers/verifySignature';
-import { leafCertToASN1Object, findOID, ASN1Object, JASN1 } from '../../helpers/asn1Utils';
 import convertCOSEtoPKCS, { COSEALGHASH } from '../../helpers/convertCOSEtoPKCS';
 import parseCertificateASN1 from '../../helpers/parseCertificateBuffer';
 import MetadataService from '../../metadata/metadataService';
@@ -31,12 +33,9 @@ export default async function verifyAttestationAndroidKey(options: Options): Pro
     throw new Error(`Attestation statement did not contain alg (AndroidKey)`);
   }
 
-  const certASN1 = leafCertToASN1Object(x5c[0]);
-
   // Check that credentialPublicKey matches the public key in the attestation certificate
   // Find the public cert in the certificate as PKCS
   const parsedCert = parseCertificateASN1(x5c[0]);
-  console.log('extensions:', parsedCert.tbsCertificate.extensions);
   const parsedCertPubKey = Buffer.from(
     parsedCert.tbsCertificate.subjectPublicKeyInfo.subjectPublicKey,
   );
@@ -49,29 +48,31 @@ export default async function verifyAttestationAndroidKey(options: Options): Pro
   }
 
   // Find Android KeyStore Extension in certificate extensions
-  const extKeyStore = getASN1ExtKeyStore(certASN1);
+  const extKeyStore = parsedCert.tbsCertificate.extensions?.find(
+    ext => ext.extnID === id_ce_keyDescription,
+  );
 
   if (!extKeyStore) {
     throw new Error('Certificate did not contain extKeyStore (AndroidKey)');
   }
 
-  // Verify extKeyStore values
-  const { attestationChallenge, teeEnforced, softwareEnforced } = extKeyStore;
+  const parsedExtKeyStore = AsnParser.parse(extKeyStore.extnValue, KeyDescription);
 
-  if (!attestationChallenge.equals(clientDataHash)) {
+  // Verify extKeyStore values
+  const { attestationChallenge, teeEnforced, softwareEnforced } = parsedExtKeyStore;
+
+  if (!Buffer.from(attestationChallenge).equals(clientDataHash)) {
     throw new Error('Attestation challenge was not equal to client data hash (AndroidKey)');
   }
 
   // Ensure that the key is strictly bound to the caller app identifier (shouldn't contain the
-  // following tag)
-  const allApplicationsTag = '[600]';
-
-  if (teeEnforced.indexOf(allApplicationsTag) >= 0) {
-    throw new Error('teeEnforced contained "[600]" tag (AndroidKey)');
+  // [600] tag)
+  if (teeEnforced.allApplications !== undefined) {
+    throw new Error('teeEnforced contained "allApplications [600]" tag (AndroidKey)');
   }
 
-  if (softwareEnforced.indexOf(allApplicationsTag) >= 0) {
-    throw new Error('teeEnforced contained "[600]" tag (AndroidKey)');
+  if (softwareEnforced.allApplications !== undefined) {
+    throw new Error('teeEnforced contained "allApplications [600]" tag (AndroidKey)');
   }
 
   // TODO: Confirm that the root certificate is an expected certificate
@@ -104,45 +105,6 @@ type KeyStoreExtensionDescription = {
   softwareEnforced: string[];
   teeEnforced: string[];
 };
-
-function getASN1ExtKeyStore(certASN1: ASN1Object): KeyStoreExtensionDescription | undefined {
-  const oid = '1.3.6.1.4.1.11129.2.1.17';
-  const ext = findOID(certASN1, oid);
-
-  if (!ext) {
-    return;
-  }
-
-  const description = (ext.data as JASN1[])[1];
-  const descData = (description.data as JASN1[])[0].data;
-
-  if (!descData) {
-    return;
-  }
-
-  /**
-   * Cast to number according to RFC 5280
-   * https://tools.ietf.org/html/rfc5280#section-3.1
-   */
-  const rawAttestationVersion = (descData[0] as JASN1).data as string;
-  let attestationVersion = 1;
-  if (rawAttestationVersion === '1') {
-    attestationVersion = 2;
-  } else if (rawAttestationVersion === '2') {
-    attestationVersion = 3;
-  }
-
-  const attestationChallenge = (descData[4] as JASN1).data as Buffer;
-  const softwareEnforced = ((descData[6] as JASN1).data as JASN1[]).map(data => data.type);
-  const teeEnforced = ((descData[7] as JASN1).data as JASN1[]).map(data => data.type);
-
-  return {
-    attestationVersion,
-    attestationChallenge,
-    softwareEnforced,
-    teeEnforced,
-  };
-}
 
 // TODO: Find the most up-to-date expected root cert, the one from Yuriy's article doesn't match
 const expectedRootCert = ``;
