84b2e6261ae12aa5e280d2a54b6477f23e495549
Matthew Miller <matthew@millerti.me>
2020-07-11 00:39:45 -0700

    Tweak some wording in example RP

--- example/index.js
+++ example/index.js
@@ -53,10 +53,10 @@ app.use(express.json());
 const rpID = 'dev.yourdomain.com';
 const origin = `https://${rpID}`;
 /**
- * WebAuthn expects you to be able to uniquely identify the user that performs an attestation or
- * assertion. The user ID you specify here should be your internal, _unique_ ID for that user
- * (uuid, etc...). Avoid using identifying information here, like email addresses, as it may be
- * stored within the authenticator.
+ * 2FA and Passwordless WebAuthn flows expect you to be able to uniquely identify the user that
+ * performs an attestation or assertion. The user ID you specify here should be your internal,
+ * _unique_ ID for that user (uuid, etc...). Avoid using identifying information here, like email
+ * addresses, as it may be stored within the authenticator.
  *
  * Here, the example server assumes the following user has completed login:
  */
