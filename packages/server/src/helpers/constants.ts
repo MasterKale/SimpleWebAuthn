68b532c40c659649a02d94c193160e2f5692dfe9
Matthew Miller <matthew@millerti.me>
2020-07-12 11:20:29 -0700

    Remove unused env vars from Server

--- packages/server/src/helpers/constants.ts
+++ packages/server/src/helpers/constants.ts
@@ -1,25 +1,3 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const { ENABLE_MDS, MDS_API_TOKEN, MDS_TOC_URL, MDS_ROOT_CERT_URL } = process.env;
-
-/**
- * Supported environment variables:
- *
- * @prop `ENABLE_MDS`: Enable support for the FIDO Metadata Service API
- * @prop `MDS_API_TOKEN`: FIDO Metadata Service API token (see https://fidoalliance.org/metadata/)
- * @prop `MDS_TOC_URL`: Alternative URL to the FIDO Metadata Service TOC endpoint (defaults to
- * https://mds2.fidoalliance.org/)
- * @prop `MDS_ROOT_CERT_URL`: URL to root certificate for completing certificate chains
- */
-export const ENV_VARS = {
-  ENABLE_MDS: ENABLE_MDS === 'true' ? true : false,
-  MDS_API_TOKEN: MDS_API_TOKEN || '',
-  MDS_TOC_URL: MDS_TOC_URL || 'https://mds2.fidoalliance.org/',
-  MDS_ROOT_CERT_URL: MDS_ROOT_CERT_URL || 'https://mds.fidoalliance.org/Root.cer',
-};
-
 type COSEInfo = {
   kty: number;
   alg: number;
