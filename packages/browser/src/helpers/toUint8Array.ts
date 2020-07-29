a457a4aaf930ded31ec9454e64ace95361c6477e
Matthew Miller <matthew@millerti.me>
2020-07-28 18:13:57 -0700

    Use TextEncoder to encode UTF-8 to a buffer

--- packages/browser/src/helpers/toUint8Array.ts
+++ packages/browser/src/helpers/toUint8Array.ts
@@ -1,7 +1,9 @@
+const utf8Encoder = new TextEncoder();
+
 /**
  * A helper method to convert an arbitrary string sent from the server to a Uint8Array the
  * authenticator will expect.
  */
 export default function toUint8Array(value: string): Uint8Array {
-  return Uint8Array.from(value, c => c.charCodeAt(0));
+  return utf8Encoder.encode(value);
 }
