db62e731ecc211f09e43c4ff4c39184e3a513587
Matthew Miller <matthew@millerti.me>
2020-07-30 18:02:12 -0700

    Add unittests for generateChallenge()

---
+++ packages/server/src/helpers/generateChallenge.test.ts
@@ -0,0 +1,14 @@
+import generateChallenge from './generateChallenge';
+
+test('should return a buffer of at least 32 bytes', () => {
+  const challenge = generateChallenge();
+
+  expect(challenge.byteLength).toBeGreaterThanOrEqual(32);
+});
+
+test('should return random bytes on each execution', () => {
+  const challenge1 = generateChallenge();
+  const challenge2 = generateChallenge();
+
+  expect(challenge1).not.toEqual(challenge2);
+});
