c26054efbe2da54241a03153526796c3841440ab
Matthew Miller <matthew@millerti.me>
2020-09-01 22:53:59 -0700

    Export new debugging methods

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -8,6 +8,8 @@ import verifyAttestationResponse from './attestation/verifyAttestationResponse';
 import generateAssertionOptions from './assertion/generateAssertionOptions';
 import verifyAssertionResponse from './assertion/verifyAssertionResponse';
 import MetadataService from './metadata/metadataService';
+import previewAttestation from './debugging/previewAttestation';
+import previewAssertion from './debugging/previewAssertion';
 
 export {
   generateAttestationOptions,
@@ -15,4 +17,7 @@ export {
   generateAssertionOptions,
   verifyAssertionResponse,
   MetadataService,
+  // Debugging tools
+  previewAttestation,
+  previewAssertion,
 };
