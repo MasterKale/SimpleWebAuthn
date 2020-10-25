5c34f5069aea47bdb936a46b5f8dd92aa3d3eec8
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2020-10-25 15:15:51 -0600

    chore: Switch to typedoc@beta for documentation generation

--- typedoc-plugin-external-module-name/index.js
+++ typedoc-plugin-external-module-name/index.js
@@ -1,6 +1,20 @@
+//@ts-check
+
 /* eslint-disable @typescript-eslint/no-var-requires */
-var plugin = require('./typedoc-plugin-external-module-name');
-module.exports = function(PluginHost) {
-  var app = PluginHost.owner;
-  app.converter.addComponent('external-module-name', plugin.ExternalModuleNamePlugin);
+const { ReflectionKind } = require('typedoc');
+const { Converter } = require('typedoc/dist/lib/converter');
+
+/** @param {import("typedoc/dist/lib/utils/plugins").PluginHost} host */
+exports.load = function (host) {
+  host.application.converter.on(Converter.EVENT_RESOLVE_BEGIN, context => {
+    /** @type {import("typedoc").ProjectReflection} */
+    const project = context.project;
+
+    for (const mod of project.children.filter(child => child.kind === ReflectionKind.Module)) {
+      const tag = mod.comment?.getTag('module');
+      if (!tag) continue;
+      mod.name = tag.text;
+      mod.comment.removeTags('module');
+    }
+  });
 };
