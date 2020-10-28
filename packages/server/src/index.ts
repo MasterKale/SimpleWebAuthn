0a46071bb5cd869b8a6f6fa63915486c74fecb9c
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2020-10-27 19:05:54 -0600

    chore: Remove unnecessary @preferred tag

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,6 @@
 /**
  * @packageDocumentation
  * @module @simplewebauthn/server
- * @preferred
  */
 import generateAttestationOptions from './attestation/generateAttestationOptions';
 import verifyAttestationResponse from './attestation/verifyAttestationResponse';
