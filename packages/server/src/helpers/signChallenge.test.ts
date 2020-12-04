85fd6e08561742b3b070626e96832cedc371d2ac
Antoine Cormouls <contact.antoine.cormouls@gmail.com>
2020-12-04 18:49:32 +0100

    fix test

--- packages/server/src/helpers/signChallenge.test.ts
+++ packages/server/src/helpers/signChallenge.test.ts
@@ -1,20 +1,26 @@
-import { verify } from 'jsonwebtoken';
 import signChallenge from './signChallenge';
+import verifyChallenge from './verifyChallenge';
 
 test('should sign challenge', () => {
   const serverSecret = '17hMcXI0AvkM7f4OWxBPwRE30D6HnoFBHAJT8Wt6AnbOh0Y9X2sXERpXaavEVEDH';
+  const payload = { challenge: 'test', rpID: 'test', origin: 'test' };
   try {
-    signChallenge('test', 'test');
+    signChallenge(payload, 'test');
     fail();
   } catch (e) {
-    expect(e).toContain('serverSecret seems too weak please use a secret with more than 64 chars');
+    expect(e.message).toContain(
+      'serverSecret seems too weak please use a secret with more than 64 chars',
+    );
   }
 
-  expect(signChallenge('test')).toEqual(undefined);
+  expect(signChallenge(payload)).toEqual(undefined);
 
-  const verifiedChallenge = verify(signChallenge('test', serverSecret), serverSecret) as {
-    challenge: string;
-  };
+  const verifiedChallenge = verifyChallenge(
+    signChallenge(payload, serverSecret) as string,
+    serverSecret,
+  );
 
   expect(verifiedChallenge.challenge).toEqual('test');
+  expect(verifiedChallenge.rpID).toEqual('test');
+  expect(verifiedChallenge.origin).toEqual('test');
 });
