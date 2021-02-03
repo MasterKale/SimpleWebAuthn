db9881eff5598f64610f74568f8196450dfb4f4f
Matthew Miller <matthew@millerti.me>
2021-02-03 15:23:00 -0800

    Update return value from assertion verification

--- packages/server/src/assertion/verifyAssertionResponse.ts
+++ packages/server/src/assertion/verifyAssertionResponse.ts
@@ -194,8 +194,8 @@ export default function verifyAssertionResponse(options: Options): VerifiedAsser
   const toReturn = {
     verified: verifySignature(signature, signatureBase, publicKey),
     authenticatorInfo: {
-      counter,
-      base64CredentialID: credential.id,
+      newCounter: counter,
+      credentialID: authenticator.credentialID,
     },
   };
 
@@ -206,16 +206,17 @@ export default function verifyAssertionResponse(options: Options): VerifiedAsser
  * Result of assertion verification
  *
  * @param verified If the assertion response could be verified
- * @param authenticatorInfo.base64CredentialID The ID of the authenticator used during assertion.
+ * @param authenticatorInfo.credentialID The ID of the authenticator used during assertion.
  * Should be used to identify which DB authenticator entry needs its `counter` updated to the value
  * below
- * @param authenticatorInfo.counter The number of times the authenticator identified above reported
- * it has been used. **Should be kept in a DB for later reference to help prevent replay attacks!**
+ * @param authenticatorInfo.newCounter The number of times the authenticator identified above
+ * reported it has been used. **Should be kept in a DB for later reference to help prevent replay
+ * attacks!**
  */
 export type VerifiedAssertion = {
   verified: boolean;
   authenticatorInfo: {
-    counter: number;
-    base64CredentialID: string;
+    credentialID: Buffer;
+    newCounter: number;
   };
 };
