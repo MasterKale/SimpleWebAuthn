22b1437ecc8bc870296697c6b3b7c4d35e509590
Matthew Miller <matthew@millerti.me>
2021-04-01 07:36:23 -0700

    Set up Babel for unbundled ESM build

---
+++ packages/browser/.babelrc.js
@@ -0,0 +1,23 @@
+const sharedPresets = ['@babel/typescript'];
+const shared = {
+  ignore: ['src/**/*.test.ts'],
+  presets: sharedPresets,
+};
+
+module.exports = {
+  env: {
+    esmUnbundled: shared,
+    esmBundled: {
+      ...shared,
+      presets: [
+        [
+          '@babel/env',
+          {
+            targets: '> 0.25%, not dead',
+          },
+        ],
+        ...sharedPresets,
+      ],
+    },
+  },
+};
