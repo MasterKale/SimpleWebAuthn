046e2fc4393da1906f35ac9e48847d646c316091
Matthew Miller <matthew@millerti.me>
2021-04-01 21:33:33 -0700

    Add version comment injection

--- packages/browser/rollup.config.js
+++ packages/browser/rollup.config.js
@@ -1,6 +1,7 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import versionInjector from 'rollup-plugin-version-injector';
 
 const extensions = ['.ts'];
 
@@ -39,6 +40,14 @@ export default {
       include: ['src/**/*.ts'],
       extensions,
       exclude: './node_modules/**',
+    }),
+    versionInjector({
+      injectInComments: {
+        fileRegexp: /\.(js)$/,
+        // [@simplewebauthn/browser]  Version: 2.1.0 - Saturday, February 6th, 2021, 4:10:31 PM
+        tag: '/* [@simplewebauthn/browser]  Version: {version} - {date} */',
+        dateFormat: 'dddd, mmmm dS, yyyy, h:MM:ss TT',
+      },
     }),
   ],
 };
