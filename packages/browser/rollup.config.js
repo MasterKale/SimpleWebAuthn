d719fe0ce9bce2e95c53dced1473da3dbf9defee
Matthew Miller <matthew@millerti.me>
2021-04-04 22:21:59 -0700

    Update rollup to just build UMD bundle

--- packages/browser/rollup.config.js
+++ packages/browser/rollup.config.js
@@ -1,30 +1,30 @@
-import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import versionInjector from 'rollup-plugin-version-injector';
 
-const extensions = ['.ts'];
-
 export default {
   input: 'src/index.ts',
+  // input: 'dist/es5/index.js',
   output: [
-    {
-      file: 'dist/bundles/bundle.esm.js',
-      format: 'esm',
-      sourcemap: true,
-    },
-    {
-      file: 'dist/bundles/bundle.esm.min.js',
-      format: 'esm',
-      plugins: [terser()],
-      sourcemap: true,
-    },
-    {
-      file: 'dist/bundles/bundle.umd.js',
-      format: 'umd',
-      name: 'SimpleWebAuthnBrowser',
-      sourcemap: true,
-    },
+    // {
+    //   file: 'dist/bundles/bundle.esm.js',
+    //   format: 'esm',
+    //   sourcemap: true,
+    // },
+    // {
+    //   file: 'dist/bundles/bundle.esm.min.js',
+    //   format: 'esm',
+    //   sourcemap: true,
+    //   plugins: [terser()],
+    // },
+    // {
+    //   file: 'dist/bundles/bundle.umd.js',
+    //   format: 'umd',
+    //   name: 'SimpleWebAuthnBrowser',
+    //   sourcemap: true,
+    // },
     {
       file: 'dist/bundles/bundle.umd.min.js',
       format: 'umd',
@@ -34,18 +34,12 @@ export default {
     },
   ],
   plugins: [
-    resolve({ extensions }),
-    babel({
-      babelHelpers: 'bundled',
-      include: ['src/**/*.ts'],
-      extensions,
-      exclude: './node_modules/**',
-    }),
+    typescript({ tsconfig: './tsconfig.es5.json' }),
     versionInjector({
       injectInComments: {
         fileRegexp: /\.(js)$/,
         // [@simplewebauthn/browser]  Version: 2.1.0 - Saturday, February 6th, 2021, 4:10:31 PM
-        tag: '/* [@simplewebauthn/browser]  Version: {version} - {date} */',
+        tag: '[@simplewebauthn/browser]  Version: {version} - {date}',
         dateFormat: 'dddd, mmmm dS, yyyy, h:MM:ss TT',
       },
     }),
