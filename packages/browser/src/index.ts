72085de0b16c2467af32a45fa8c86cd531a632eb
Matthew Miller <matthew@millerti.me>
2021-04-05 21:38:27 -0700

    Revert `export default` from index.ts

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -6,4 +6,4 @@ import startAttestation from './methods/startAttestation';
 import startAssertion from './methods/startAssertion';
 import supportsWebauthn from './helpers/supportsWebauthn';
 
-export default { startAttestation, startAssertion, supportsWebauthn };
+export { startAttestation, startAssertion, supportsWebauthn };
