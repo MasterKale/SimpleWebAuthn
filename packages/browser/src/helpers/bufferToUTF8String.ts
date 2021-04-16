032e8cb32467c140e945dd71b868828b9fb4a259
Matthew Miller <matthew@millerti.me>
2021-04-15 21:35:05 -0700

    Add bufferToUTF8String

---
+++ packages/browser/src/helpers/bufferToUTF8String.ts
@@ -0,0 +1,7 @@
+/**
+ * A helper method to convert an arbitrary ArrayBuffer, returned from an authenticator, to a UTF-8
+ * string.
+ */
+export default function bufferToUTF8String(value: ArrayBuffer): string {
+  return new TextDecoder('utf-8').decode(value);
+}
