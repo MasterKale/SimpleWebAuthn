f994527c21b27fb9c86374a3c214374be9ec3623
Matthew Miller <matthew@millerti.me>
2021-08-20 21:31:09 -0700

    Add method to convert from algSign to COSE info

--- packages/server/src/metadata/verifyAttestationWithMetadata.ts
+++ packages/server/src/metadata/verifyAttestationWithMetadata.ts
@@ -1,7 +1,6 @@
 import { Base64URLString } from '@simplewebauthn/typescript-types';
 
 import { MetadataStatement } from '../metadata/mdsTypes';
-import { FIDO_METADATA_AUTH_ALG_TO_COSE } from '../helpers/constants';
 import convertCertBufferToPEM from '../helpers/convertCertBufferToPEM';
 import validateCertificatePath from '../helpers/validateCertificatePath';
 
@@ -26,4 +25,52 @@ export default async function verifyAttestationWithMetadata(
   }
 
   return true;
+}
+
+type COSEInfo = {
+  kty: number;
+  alg: number;
+  crv?: number;
+};
+
+/**
+ * Convert ALG_SIGN values to COSE info
+ * https://fidoalliance.org/specs/common-specs/fido-registry-v2.1-ps-20191217.html#authentication-algorithms
+ */
+function algSignToCOSEInfo(algSign: AlgSign): COSEInfo | undefined {
+  switch (algSign) {
+    case 'secp256r1_ecdsa_sha256_raw':
+    case 'secp256r1_ecdsa_sha256_der':
+      return { kty: 2, alg: -7, crv: 1 };
+    case 'rsassa_pss_sha256_raw':
+    case 'rsassa_pss_sha256_der':
+      return { kty: 3, alg: -37 };
+    case 'secp256k1_ecdsa_sha256_raw':
+    case 'secp256k1_ecdsa_sha256_der':
+      return { kty: 2, alg: -7, crv: 8 };
+    case 'rsassa_pss_sha384_raw':
+      return { kty: 3, alg: -38 };
+    case 'rsassa_pkcsv15_sha256_raw':
+      return { kty: 3, alg: -257 };
+    case 'rsassa_pkcsv15_sha384_raw':
+      return { kty: 3, alg: -258 };
+    case 'rsassa_pkcsv15_sha512_raw':
+      return { kty: 3, alg: -259 };
+    case 'rsassa_pkcsv15_sha1_raw':
+      return { kty: 3, alg: -65535 };
+    case 'secp384r1_ecdsa_sha384_raw':
+      return { kty: 2, alg: -35, crv: 2 };
+    case 'secp512r1_ecdsa_sha256_raw':
+      return { kty: 2, alg: -36, crv: 3 };
+    case 'ed25519_eddsa_sha512_raw':
+      return { kty: 1, alg: -8, crv: 6 };
+    // TODO: COSE info in FIDO Registry v2.1 isn't readily available for these, these seem rare...
+    // case 'sm2_sm3_raw':
+    //   return {};
+    // case 'rsa_emsa_pkcs1_sha256_raw':
+    // case 'rsa_emsa_pkcs1_sha256_der':
+    //   return {};
+    default:
+      return undefined;
+  }
 }
