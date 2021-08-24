fb79f6941a1be0a4c320bb91079e109ee01cd5ad
Matthew Miller <matthew@millerti.me>
2021-08-23 21:08:38 -0700

    Export new helper method

--- packages/browser/src/index.test.ts
+++ packages/browser/src/index.test.ts
@@ -11,3 +11,7 @@ test('should export method `startAssertion`', () => {
 test('should export method `browserSupportsWebauthn`', () => {
   expect(index.browserSupportsWebauthn).toBeDefined();
 });
+
+test('should export method `platformAuthenticatorIsAvailable`', () => {
+  expect(index.browserSupportsWebauthn).toBeDefined();
+});
--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -5,5 +5,11 @@
 import startAttestation from './methods/startAttestation';
 import startAssertion from './methods/startAssertion';
 import { browserSupportsWebauthn } from './helpers/browserSupportsWebauthn';
+import { platformAuthenticatorIsAvailable } from './helpers/platformAuthenticatorIsAvailable';
 
-export { startAttestation, startAssertion, browserSupportsWebauthn };
+export {
+  startAttestation,
+  startAssertion,
+  browserSupportsWebauthn,
+  platformAuthenticatorIsAvailable,
+};
