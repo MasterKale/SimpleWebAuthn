653ae6db15126ed20dfe3a11697b6d2f8d9a3eda
TaigaMikami <mikamimikami60@gmail.com>
2022-04-10 13:15:29 +0900

    add toHash function test

---
+++ packages/server/src/helpers/toHash.test.ts
@@ -0,0 +1,11 @@
+import toHash from './toHash';
+
+test('should return a buffer of at 32 bytes for input string', () => {
+  const hash = toHash("string");
+  expect(hash.byteLength).toEqual(32);
+});
+
+test('should return a buffer of at 32 bytes for input Buffer', () => {
+  const hash = toHash(new Buffer(10));
+  expect(hash.byteLength).toEqual(32);
+});
