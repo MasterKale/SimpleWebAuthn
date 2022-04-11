da3c7005c4f4ffd90ede382bcd329ae2aaa5d2bd
TaigaMikami <mikamimikami60@gmail.com>
2022-04-11 21:21:21 +0900

    check  the PEM certificate only have 64 characters per line

--- packages/server/src/helpers/convertCertBufferToPEM.test.ts
+++ packages/server/src/helpers/convertCertBufferToPEM.test.ts
@@ -1,13 +1,38 @@
 import convertCertBufferToPEM from "./convertCertBufferToPEM";
 
 test('should return pem when input is base64URLString', () => {
-  const input = "Y2VydEJ1ZmZlclN0cmluZw";
+  const input = "Y2VydEJ1ZmZlclN0cmluZyBjZXJ0QnVmZmVyU3RyaW5nIGNlcnRCdWZmZXJTdHJpbmcgY2VydEJ1ZmZlclN0cmluZyBjZXJ0QnVmZmVyU3RyaW5nIGNlcnRCdWZmZXJTdHJpbmcgY2VydEJ1ZmZlclN0cmluZw";
   const actual = convertCertBufferToPEM(input);
-  expect(actual).toEqual(`-----BEGIN CERTIFICATE-----\nY2VydEJ1ZmZlclN0cmluZw==\n-----END CERTIFICATE-----\n`)
+  const actualPemArr = actual.split("\n");
+
+  expect(actual).toEqual(`-----BEGIN CERTIFICATE-----
+Y2VydEJ1ZmZlclN0cmluZyBjZXJ0QnVmZmVyU3RyaW5nIGNlcnRCdWZmZXJTdHJp
+bmcgY2VydEJ1ZmZlclN0cmluZyBjZXJ0QnVmZmVyU3RyaW5nIGNlcnRCdWZmZXJT
+dHJpbmcgY2VydEJ1ZmZlclN0cmluZw==
+-----END CERTIFICATE-----
+`);
+
+  expect(actualPemArr[0]).toEqual("-----BEGIN CERTIFICATE-----");
+  expect(actualPemArr[1].length).toBeLessThanOrEqual(64);
+  expect(actualPemArr[2].length).toBeLessThanOrEqual(64);
+  expect(actualPemArr[3].length).toBeLessThanOrEqual(64);
+  expect(actualPemArr[4]).toEqual("-----END CERTIFICATE-----");
 });
 
 test('should return pem when input is buffer', () => {
-  const input = new Buffer(10);
+  const input = new Buffer(128);
   const actual = convertCertBufferToPEM(input);
-  expect(actual).toEqual(`-----BEGIN CERTIFICATE-----\nAAAAAAAAAAAAAA==\n-----END CERTIFICATE-----\n`)
+  const actualPemArr = actual.split("\n");
+  expect(actual).toEqual(`-----BEGIN CERTIFICATE-----
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=
+-----END CERTIFICATE-----
+`);
+
+  expect(actualPemArr[0]).toEqual("-----BEGIN CERTIFICATE-----");
+  expect(actualPemArr[1].length).toBeLessThanOrEqual(64);
+  expect(actualPemArr[2].length).toBeLessThanOrEqual(64);
+  expect(actualPemArr[3].length).toBeLessThanOrEqual(64);
+  expect(actualPemArr[4]).toEqual("-----END CERTIFICATE-----");
 });
