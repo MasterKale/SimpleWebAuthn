e0309e69658a364d966354eccf006977129745ac
Matthew Miller <matthew@millerti.me>
2022-04-13 09:15:22 -0700

    Add tests for parseBackupFlags

---
+++ packages/server/src/helpers/parseBackupFlags.test.ts
@@ -0,0 +1,33 @@
+import { parseBackupFlags } from './parseBackupFlags';
+
+test('should return single-device cred, not backed up', () => {
+  const parsed = parseBackupFlags({ be: false, bs: false });
+
+  expect(parsed.credentialDeviceType).toEqual('singleDevice');
+  expect(parsed.credentialBackedUp).toEqual(false);
+});
+
+test('should throw on single-device cred, backed up', () => {
+  expect.assertions(2);
+
+  try {
+    parseBackupFlags({ be: false, bs: true });
+  } catch (err) {
+    expect(err.message).toContain('impossible');
+    expect(err.name).toEqual('InvalidBackupFlags')
+  }
+});
+
+test('should return multi-device cred, not backed up', () => {
+  const parsed = parseBackupFlags({ be: true, bs: false });
+
+  expect(parsed.credentialDeviceType).toEqual('multiDevice');
+  expect(parsed.credentialBackedUp).toEqual(false);
+});
+
+test('should return multi-device cred, backed up', () => {
+  const parsed = parseBackupFlags({ be: true, bs: true });
+
+  expect(parsed.credentialDeviceType).toEqual('multiDevice');
+  expect(parsed.credentialBackedUp).toEqual(true);
+});
