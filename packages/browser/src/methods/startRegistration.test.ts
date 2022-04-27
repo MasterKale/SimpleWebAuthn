a40c03ec385a4cdf63a2c25a60b4ca726a30d301
Matthew Miller <matthew@millerti.me>
2022-04-27 09:39:44 -0700

    Add tests for new transport

--- packages/browser/src/methods/startRegistration.test.ts
+++ packages/browser/src/methods/startRegistration.test.ts
@@ -176,6 +176,23 @@ test('should include extension results when no extensions specified', async () =
   expect(response.clientExtensionResults).toEqual({});
 });
 
+test('should support "cable" transport', async () => {
+  const opts: PublicKeyCredentialCreationOptionsJSON = {
+    ...goodOpts1,
+    excludeCredentials: [
+      {
+        ...goodOpts1.excludeCredentials![0],
+        transports: ["cable"],
+      },
+    ]
+  };
+
+  await startRegistration(opts);
+
+  expect(mockNavigatorCreate.mock.calls[0][0].publicKey.excludeCredentials[0].transports[0])
+    .toEqual("cable");
+});
+
 describe('WebAuthnError', () => {
   describe('AbortError', () => {
     const AbortError = generateCustomError('AbortError');
