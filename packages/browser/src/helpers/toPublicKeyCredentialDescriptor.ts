d66925d6f6b706d4c8f024481953f5e09f8e2b0f
Matthew Miller <matthew@millerti.me>
2022-04-27 09:24:20 -0700

    Add "cable" support

--- packages/browser/src/helpers/toPublicKeyCredentialDescriptor.ts
+++ packages/browser/src/helpers/toPublicKeyCredentialDescriptor.ts
@@ -10,5 +10,11 @@ export default function toPublicKeyCredentialDescriptor(
   return {
     ...descriptor,
     id: base64URLStringToBuffer(id),
+    /**
+     * `descriptor.transports` is an array of our `AuthenticatorTransport` that includes newer
+     * transports that TypeScript's DOM lib is ignorant of. Convince TS that our list of transports
+     * are fine to pass to WebAuthn since browsers will recognize the new value.
+     */
+     transports: descriptor.transports as AuthenticatorTransport[],
   };
 }
