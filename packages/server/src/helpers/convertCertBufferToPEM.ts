cc0484540258811056cbf73816917e0af77e4cfa
Matthew Miller <matthew@millerti.me>
2022-08-14 11:31:11 -0700

    Reduce string -> buffer -> string action

--- packages/server/src/helpers/convertCertBufferToPEM.ts
+++ packages/server/src/helpers/convertCertBufferToPEM.ts
@@ -5,14 +5,16 @@ import type { Base64URLString } from '@simplewebauthn/typescript-types';
  * Convert buffer to an OpenSSL-compatible PEM text format.
  */
 export function convertCertBufferToPEM(certBuffer: Buffer | Base64URLString): string {
-  let buffer: Buffer;
-  if (typeof certBuffer === 'string') {
-    buffer = base64url.toBuffer(certBuffer);
-  } else {
-    buffer = certBuffer;
-  }
+  let b64cert: string;
 
-  const b64cert = buffer.toString('base64');
+  /**
+   * Get certBuffer to a base64 representation
+   */
+  if (typeof certBuffer === 'string') {
+    b64cert = base64url.toBase64(certBuffer);
+  } else {
+    b64cert = certBuffer.toString('base64');
+  }
 
   let PEMKey = '';
   for (let i = 0; i < Math.ceil(b64cert.length / 64); i += 1) {
