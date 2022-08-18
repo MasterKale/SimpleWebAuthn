078037e46d7f3d1f31568b34c616446bd1837bb4
Eiji Kitamura <agektmr@google.com>
2022-08-18 21:22:41 +0900

    Add temporary attestation verification

--- packages/server/src/extensions/devicePubKey.ts
+++ packages/server/src/extensions/devicePubKey.ts
@@ -1,7 +1,16 @@
 import base64url from "base64url";
 import { toHash } from "../helpers/toHash";
 import { verifySignature } from "../helpers/verifySignature";
-import { DevicePublicKeyAuthenticatorOutput } from "helpers/decodeAuthenticatorExtensions";
+import { DevicePublicKeyAuthenticatorOutput } from "../helpers/decodeAuthenticatorExtensions";
+import { SettingsService } from "../services/settingsService";
+import { AttestationFormatVerifierOpts } from "../registration/verifyRegistrationResponse";
+import { parseAuthenticatorData } from "../helpers/parseAuthenticatorData";
+import { verifyAttestationFIDOU2F } from '../registration/verifications/verifyAttestationFIDOU2F';
+import { verifyAttestationPacked } from '../registration/verifications/verifyAttestationPacked';
+import { verifyAttestationAndroidSafetyNet } from '../registration/verifications/verifyAttestationAndroidSafetyNet';
+import { verifyAttestationTPM } from '../registration//verifications/tpm/verifyAttestationTPM';
+import { verifyAttestationAndroidKey } from '../registration/verifications/verifyAttestationAndroidKey';
+import { verifyAttestationApple } from '../registration/verifications/verifyAttestationApple';
 
 /**
  * https://pr-preview.s3.amazonaws.com/w3c/webauthn/pull/1663.html#sctn-device-publickey-extension-verification-create
@@ -16,18 +25,67 @@ import { DevicePublicKeyAuthenticatorOutput } from "helpers/decodeAuthenticatorE
  * @returns Promise<boolean>
  */
 export async function verifyDpkSignature(
-  credentialID: string,
   clientDataJSON: string,
+  authData: Buffer,
   devicePubKey: DevicePublicKeyAuthenticatorOutput,
   signature: Buffer,
 ): Promise<boolean> {
-  const rawId = base64url.toBuffer(credentialID);
   const clientDataHash = toHash(base64url.toBuffer(clientDataJSON));
+  const parsedAuthData = parseAuthenticatorData(authData);
+  const { rpIdHash, credentialID } = parsedAuthData;
+  
+  if (!credentialID) {
+    // `authData` without `credentialID` can't be verified.
+    return false;
+  }
+
   const nonce = devicePubKey.nonce ? devicePubKey.nonce : Buffer.from('');
-  const signatureBase = Buffer.concat([rawId, clientDataHash, nonce]);
+  const signatureBase = Buffer.concat([credentialID, clientDataHash, nonce]);
   const credentialPublicKey = devicePubKey.dpk;
 
-  // TODO: Implement a logic to verify attestation signatures
+  // If `fmt` and `attStmt` are not included, skip verification.
+  if (devicePubKey.fmt && devicePubKey.attStmt) {
+    const rootCertificates = SettingsService.getRootCertificates({ identifier: devicePubKey.fmt });
+
+    // Prepare arguments to pass to the relevant verification method
+    const verifierOpts: AttestationFormatVerifierOpts = {
+      aaguid: devicePubKey.aaguid,
+      attStmt: devicePubKey.attStmt,
+      authData,
+      clientDataHash,
+      credentialID,
+      credentialPublicKey,
+      rootCertificates,
+      rpIdHash,
+    };
+
+    // TODO: Implement logics to verify attestation signatures
+    let verified = false;
+    if (devicePubKey.fmt === 'fido-u2f') {
+      verified = await verifyAttestationFIDOU2F(verifierOpts);
+    } else if (devicePubKey.fmt === 'packed') {
+      verified = await verifyAttestationPacked(verifierOpts);
+    } else if (devicePubKey.fmt === 'android-safetynet') {
+      verified = await verifyAttestationAndroidSafetyNet(verifierOpts);
+    } else if (devicePubKey.fmt === 'android-key') {
+      verified = await verifyAttestationAndroidKey(verifierOpts);
+    } else if (devicePubKey.fmt === 'tpm') {
+      verified = await verifyAttestationTPM(verifierOpts);
+    } else if (devicePubKey.fmt === 'apple') {
+      verified = await verifyAttestationApple(verifierOpts);
+    } else if (devicePubKey.fmt === 'none') {
+      if (Object.keys(devicePubKey.attStmt).length > 0) {
+        throw new Error('None attestation had unexpected attestation statement');
+      }
+      // This is the weaker of the attestations, so there's nothing else to really check
+      verified = true;
+    } else {
+      throw new Error(`Unsupported Attestation Format: ${devicePubKey.fmt}`);
+    }
+    if (!verified) {
+      return false;
+    }
+  }
 
   return verifySignature({ signature, signatureBase, credentialPublicKey });
 }
@@ -56,16 +114,13 @@ export function verifyDevicePublicKey(
   if (!devicePubKey.scope.equals(expectedDPK.scope)) {
     return false;
   }
-  if (devicePubKey.fmt &&
-      expectedDPK.fmt &&
-      devicePubKey.fmt === expectedDPK.fmt &&
-      !devicePubKey.fmt.equals(expectedDPK.fmt)) {
+  if ((devicePubKey.fmt || expectedDPK.fmt) &&
+      devicePubKey.fmt !== expectedDPK.fmt) {
     return false;
   }
-  if (devicePubKey.attStmt &&
-      expectedDPK.attStmt &&
-      devicePubKey.attStmt === expectedDPK.attStmt &&
-      !devicePubKey.attStmt.equals(expectedDPK.attStmt)) {
+  if (devicePubKey.attStmt ||
+      expectedDPK.attStmt) {
+    // TODO: Examine if comparing properties is a good idea.
     return false;
   }
   return true;
