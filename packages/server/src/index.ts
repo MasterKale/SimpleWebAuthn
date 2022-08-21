4cb1e29fdcb94eba9e49fcb62badc8a31c2efbe9
Eiji Kitamura <agektmr@google.com>
2022-08-21 18:08:13 +0900

    Reorganize the interface

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -8,9 +8,9 @@ import { generateAuthenticationOptions } from './authentication/generateAuthenti
 import { verifyAuthenticationResponse } from './authentication/verifyAuthenticationResponse';
 import { MetadataService } from './services/metadataService';
 import { SettingsService } from './services/settingsService';
-import { verifyDpkSignature } from 'extensions/devicePublicKey/verifyDpkSignature';
-import { verifyDevicePublicKey } from 'extensions/devicePublicKey/verifyDevicePublicKey';
-import { DevicePublicKeyAuthenticatorOutput } from 'helpers/decodeAuthenticatorExtensions';
+import { verifyDpkSignature } from './extensions/devicePublicKey/verifyDpkSignature';
+import { verifyDevicePublicKey } from './extensions/devicePublicKey/verifyDevicePublicKey';
+import { DevicePublicKeyAuthenticatorOutput } from './helpers/decodeAuthenticatorExtensions';
 
 export {
   generateRegistrationOptions,
