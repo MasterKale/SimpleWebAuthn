cda5d7eac10a504c1f6cf7312117abe533430abf
Eiji Kitamura <agektmr@google.com>
2022-08-21 18:08:13 +0900

    Reorganize the interface

--- packages/server/src/extensions/devicePublicKey/verifyDevicePublicKey.test.ts
+++ packages/server/src/extensions/devicePublicKey/verifyDevicePublicKey.test.ts
@@ -1,1 +1,4 @@
-// TODO: Implement a test for `verifyDevicePublicKey`
+it("should verify device public key", async () => {
+  // TODO: Implement a test for `verifyDevicePublicKey`
+  expect(true).toEqual(true);
+});
