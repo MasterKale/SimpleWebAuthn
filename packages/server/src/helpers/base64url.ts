4623774abab9a9ef861828ebfc55edd1d463df3a
Matthew Miller <matthew@millerti.me>
2022-11-11 15:32:15 -0800

    Expand base64url utils and remove default

--- packages/server/src/helpers/base64url.ts
+++ packages/server/src/helpers/base64url.ts
@@ -1,66 +1,62 @@
+import base64 from '@hexagon/base64';
+
 /**
- * Convert from a Base64URL-encoded string to an Array Buffer. Best used when converting a
+ * Decode from a Base64URL-encoded string to an ArrayBuffer. Best used when converting a
  * credential ID from a JSON string to an ArrayBuffer, like in allowCredentials or
- * excludeCredentials
+ * excludeCredentials.
  *
- * Helper method to compliment `bufferToBase64URLString`
+ * @param buffer Value to decode from base64
+ * @param to (optional) The decoding to use, in case it's desirable to decode from base64 instead
  */
-export function toBuffer(base64URLString: string): Uint8Array {
-  // Convert from Base64URL to Base64
-  const base64 = toBase64(base64URLString);
-
-  // Convert to a binary string
-  const binary = atob(base64);
-
-  // Convert binary string to buffer
-  const buffer = new ArrayBuffer(binary.length);
-  const bytes = new Uint8Array(buffer);
-
-  for (let i = 0; i < binary.length; i++) {
-    bytes[i] = binary.charCodeAt(i);
-  }
-
-  return bytes;
+export function toBuffer(base64urlString: string, from: 'base64' | 'base64url' = 'base64url'): Uint8Array {
+  const _buffer = base64.toArrayBuffer(base64urlString, from === 'base64url');
+  return new Uint8Array(_buffer);
 }
 
 /**
- * Convert the given array buffer into a Base64URL-encoded string. Ideal for converting various
+ * Encode the given array buffer into a Base64URL-encoded string. Ideal for converting various
  * credential response ArrayBuffers to string for sending back to the server as JSON.
  *
- * Helper method to compliment `base64URLStringToBuffer`
+ * @param buffer Value to encode to base64
+ * @param to (optional) The encoding to use, in case it's desirable to encode to base64 instead
  */
-export function fromBuffer(buffer: Uint8Array): string {
-  let str = '';
-
-  for (const charCode of buffer) {
-    str += String.fromCharCode(charCode);
-  }
-
-  const base64String = btoa(str);
-
-  return base64String.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+export function fromBuffer(buffer: Uint8Array, to: 'base64' | 'base64url' = 'base64url'): string {
+  return base64.fromArrayBuffer(buffer, to === 'base64url');
 }
 
 /**
  * Convert a base64url string into base64
  */
-export function toBase64(base64URLString: string): string {
-  const base64 = base64URLString.replace(/-/g, '+').replace(/_/g, '/');
-  /**
-   * Pad with '=' until it's a multiple of four
-   * (4 - (85 % 4 = 1) = 3) % 4 = 3 padding
-   * (4 - (86 % 4 = 2) = 2) % 4 = 2 padding
-   * (4 - (87 % 4 = 3) = 1) % 4 = 1 padding
-   * (4 - (88 % 4 = 0) = 4) % 4 = 0 padding
-   */
-  const padLength = (4 - (base64.length % 4)) % 4;
-  const padded = base64.padEnd(base64.length + padLength, '=');
-
-  return padded;
+export function toBase64(base64urlString: string): string {
+  const fromBase64Url = base64.toArrayBuffer(base64urlString, true);
+  const toBase64 = base64.fromArrayBuffer(fromBase64Url);
+  return toBase64;
 }
 
-export default {
-  toBuffer,
-  fromBuffer,
-  toBase64,
-};
+/**
+ * Encode a string to base64url
+ */
+export function fromString(ascii: string): string {
+  return base64.fromString(ascii, true);
+}
+
+/**
+ * Decode a base64url string into its original string
+ */
+export function toString(base64urlString: string): string {
+  return base64.toString(base64urlString, true);
+}
+
+/**
+ * Confirm that the string is encoded into base64
+ */
+export function isBase64(input: string): boolean {
+  return base64.validate(input, false);
+}
+
+/**
+ * Confirm that the string is encoded into base64url
+ */
+export function isBase64url(input: string): boolean {
+  return base64.validate(input, true);
+}
