c2061255a57283426b6618a16238621297eefbbb
Matthew Miller <matthew@millerti.me>
2022-11-20 14:32:38 -0800

    Add helper for mapping COSE alg to key alg

---
+++ packages/server/src/helpers/iso/isoCrypto/mapCoseAlgToWebCryptoKeyAlgName.ts
@@ -0,0 +1,20 @@
+import { COSEALG } from "../../cose";
+import { SubtleCryptoKeyAlgName } from "./structs";
+
+
+/**
+ * Convert a COSE alg ID into a corresponding key algorithm string value that WebCrypto APIs expect
+ */
+export function mapCoseAlgToWebCryptoKeyAlgName(alg: COSEALG): SubtleCryptoKeyAlgName {
+  if ([COSEALG.EdDSA].indexOf(alg) >= 0) {
+    return 'Ed25519';
+  } else if ([COSEALG.ES256, COSEALG.ES384, COSEALG.ES512, COSEALG.ES256K].indexOf(alg) >= 0) {
+    return 'ECDSA';
+  } else if ([COSEALG.RS256, COSEALG.RS384, COSEALG.RS512].indexOf(alg) >= 0) {
+    return 'RSASSA-PKCS1-v1_5'
+  } else if ([COSEALG.PS256, COSEALG.PS384, COSEALG.PS512].indexOf(alg) >= 0) {
+    return 'RSA-PSS';
+  }
+
+  throw new Error(`Unexpected COSE alg value of ${alg}`);
+}
