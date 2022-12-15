e021d7812b562e3bf36ef6219421d9bc77d435e5
Matthew Miller <matthew@millerti.me>
2022-12-15 09:49:43 -0800

    Map RS1 COSE alg to WebCrypto alg name

--- packages/server/src/helpers/iso/isoCrypto/mapCoseAlgToWebCryptoKeyAlgName.ts
+++ packages/server/src/helpers/iso/isoCrypto/mapCoseAlgToWebCryptoKeyAlgName.ts
@@ -9,7 +9,7 @@ export function mapCoseAlgToWebCryptoKeyAlgName(alg: COSEALG): SubtleCryptoKeyAl
     return 'Ed25519';
   } else if ([COSEALG.ES256, COSEALG.ES384, COSEALG.ES512, COSEALG.ES256K].indexOf(alg) >= 0) {
     return 'ECDSA';
-  } else if ([COSEALG.RS256, COSEALG.RS384, COSEALG.RS512].indexOf(alg) >= 0) {
+  } else if ([COSEALG.RS256, COSEALG.RS384, COSEALG.RS512, COSEALG.RS1].indexOf(alg) >= 0) {
     return 'RSASSA-PKCS1-v1_5';
   } else if ([COSEALG.PS256, COSEALG.PS384, COSEALG.PS512].indexOf(alg) >= 0) {
     return 'RSA-PSS';
