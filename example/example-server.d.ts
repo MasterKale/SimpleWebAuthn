cc7b29d4478975ce22bbdbcfbb64eedea67afdf3
Moritz Friedrich <moritz@matchory.com>
2023-01-05 18:33:42 +0100

    Add sessions to the example app

--- example/example-server.d.ts
+++ example/example-server.d.ts
@@ -35,5 +35,15 @@ interface LoggedInUser {
   id: string;
   username: string;
   devices: AuthenticatorDevice[];
-  currentChallenge?: string;
+}
+
+declare module 'express-session' {
+  interface SessionData {
+    /**
+     * A simple way of storing a user's current challenge being signed by registration or authentication.
+     * It should be expired after `timeout` milliseconds (optional argument for `generate` methods,
+     * defaults to 60000ms)
+     */
+    currentChallenge?: string;
+  }
 }
