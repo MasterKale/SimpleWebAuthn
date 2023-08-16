ee8d2a144c0d13640d0cf4548b4b2d8ebf8b6713
Matthew Miller <matthew@millerti.me>
2023-08-16 16:05:12 -0700

    Rearrange deps.ts

--- packages/server/src/deps.ts
+++ packages/server/src/deps.ts
@@ -19,11 +19,11 @@ export type {
 // @simplewebauthn/iso-webcrypto
 export { default as WebCrypto } from '../../iso-webcrypto/src/browser.ts';
 
+// cbor (a.k.a. cbor-x in Node land)
+export * as cborx from 'https://deno.land/x/cbor@v1.5.2/index.js';
+
 // NPM: cross-fetch
 export { default as fetch } from 'npm:cross-fetch';
 
 // NPM: debug
 export { default as debug, Debugger } from 'npm:debug';
-
-// cbor (a.k.a. cbor-x in Node land)
-export * as cborx from 'https://deno.land/x/cbor@v1.5.2/index.js';
