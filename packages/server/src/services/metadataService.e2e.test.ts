d6c231807ca5c36d79d09e88db23f9dcc7eb023e
Matthew Miller <matthew@millerti.me>
2023-08-17 14:42:49 -0700

    Update MetadataService E2E tests

--- packages/server/src/services/metadataService.e2e.test.ts
+++ packages/server/src/services/metadataService.e2e.test.ts
@@ -1,19 +1,19 @@
+import { assert } from "https://deno.land/std@0.198.0/assert/mod.ts";
+
 import { BaseMetadataService } from "./metadataService.ts";
 
-describe("end-to-end MetadataService tests", () => {
-  test("should be able to load from FIDO MDS and get statement for YubiKey 5", async () => {
-    const service = new BaseMetadataService();
+Deno.test("should be able to load from FIDO MDS and get statement for YubiKey 5", async () => {
+  const service = new BaseMetadataService();
 
-    await service.initialize();
+  await service.initialize();
 
-    /**
-     * From Yubico's list of AAGUIDs
-     *
-     * See https://support.yubico.com/hc/en-us/articles/360016648959-YubiKey-Hardware-FIDO2-AAGUIDs
-     */
-    const aaguidYubiKey5 = "ee882879-721c-4913-9775-3dfcce97072a";
-    const statement = await service.getStatement(aaguidYubiKey5);
+  /**
+   * From Yubico's list of AAGUIDs
+   *
+   * See https://support.yubico.com/hc/en-us/articles/360016648959-YubiKey-Hardware-FIDO2-AAGUIDs
+   */
+  const aaguidYubiKey5 = "ee882879-721c-4913-9775-3dfcce97072a";
+  const statement = await service.getStatement(aaguidYubiKey5);
 
-    expect(statement).toBeDefined();
-  });
+  assert(statement);
 });
