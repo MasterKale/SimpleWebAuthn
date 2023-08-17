abd0b64d63d5a1230554ee0eea1b5d82490ae42c
Matthew Miller <matthew@millerti.me>
2023-08-17 13:09:12 -0700

    Update toHash tests

--- packages/server/src/helpers/toHash.test.ts
+++ packages/server/src/helpers/toHash.test.ts
@@ -1,11 +1,13 @@
+import { assertEquals } from "https://deno.land/std@0.198.0/assert/mod.ts";
+
 import { toHash } from "./toHash.ts";
 
-test("should return a buffer of at 32 bytes for input string", async () => {
+Deno.test("should return a buffer of at 32 bytes for input string", async () => {
   const hash = await toHash("string");
-  expect(hash.byteLength).toEqual(32);
+  assertEquals(hash.byteLength, 32);
 });
 
-test("should return a buffer of at 32 bytes for input Buffer", async () => {
-  const hash = await toHash(Buffer.alloc(10));
-  expect(hash.byteLength).toEqual(32);
+Deno.test("should return a buffer of at 32 bytes for input Buffer", async () => {
+  const hash = await toHash(new Uint8Array(10).fill(0));
+  assertEquals(hash.byteLength, 32);
 });
