d7c7c212075dd8d4363c581d797bbebc70f95fb0
Matthew Miller <matthew@millerti.me>
2023-08-17 12:48:03 -0700

    Update convertCOSEtoPKCS tests

--- packages/server/src/helpers/convertCOSEtoPKCS.test.ts
+++ packages/server/src/helpers/convertCOSEtoPKCS.test.ts
@@ -1,28 +1,19 @@
+import { assertThrows } from "https://deno.land/std@0.198.0/assert/mod.ts";
+
 import { isoCBOR } from "./iso/index.ts";
 
 import { convertCOSEtoPKCS } from "./convertCOSEtoPKCS.ts";
 import { COSEKEYS } from "./cose.ts";
 
-test("should throw an error curve if, somehow, curve coordinate x is missing", () => {
-  const mockCOSEKey = new Map<number, number | Buffer>();
-
+Deno.test("should throw an error curve if, somehow, curve coordinate x is missing", () => {
+  const mockCOSEKey = new Map<number, number | Uint8Array>();
   mockCOSEKey.set(COSEKEYS.y, 1);
 
-  jest.spyOn(isoCBOR, "decodeFirst").mockReturnValue(mockCOSEKey);
+  const badPublicKey = isoCBOR.encode(mockCOSEKey);
 
-  expect(() => {
-    convertCOSEtoPKCS(Buffer.from("123", "ascii"));
-  }).toThrow();
-});
-
-test("should throw an error curve if, somehow, curve coordinate y is missing", () => {
-  const mockCOSEKey = new Map<number, number | Buffer>();
-
-  mockCOSEKey.set(COSEKEYS.x, 1);
-
-  jest.spyOn(isoCBOR, "decodeFirst").mockReturnValue(mockCOSEKey);
-
-  expect(() => {
-    convertCOSEtoPKCS(Buffer.from("123", "ascii"));
-  }).toThrow();
+  assertThrows(
+    () => convertCOSEtoPKCS(badPublicKey),
+    Error,
+    "public key was missing x",
+  );
 });
