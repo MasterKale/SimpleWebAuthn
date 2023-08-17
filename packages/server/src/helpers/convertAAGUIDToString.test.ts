d7dc4003316e0a765419c0acd42aba3a6da3c577
Matthew Miller <matthew@millerti.me>
2023-08-17 12:32:18 -0700

    Update convertAAGUIDToString tests

--- packages/server/src/helpers/convertAAGUIDToString.test.ts
+++ packages/server/src/helpers/convertAAGUIDToString.test.ts
@@ -1,9 +1,12 @@
-import { convertAAGUIDToString } from "./convertAAGUIDToString.ts";
+import { assertEquals } from "https://deno.land/std@0.198.0/assert/mod.ts";
 
-test("should convert buffer to UUID string", () => {
+import { convertAAGUIDToString } from "./convertAAGUIDToString.ts";
+import { isoUint8Array } from "./iso/index.ts";
+
+Deno.test("should convert buffer to UUID string", () => {
   const uuid = convertAAGUIDToString(
-    Buffer.from("adce000235bcc60a648b0b25f1f05503", "hex"),
+    isoUint8Array.fromHex("adce000235bcc60a648b0b25f1f05503"),
   );
 
-  expect(uuid).toEqual("adce0002-35bc-c60a-648b-0b25f1f05503");
+  assertEquals(uuid, "adce0002-35bc-c60a-648b-0b25f1f05503");
 });
