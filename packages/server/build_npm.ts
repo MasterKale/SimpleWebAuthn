e1d4cf710a958ad082ceb045036f287f7d7781d3
Matthew Miller <matthew@millerti.me>
2024-01-20 21:31:18 -0800

    Punt on pointing server to new package

--- packages/server/build_npm.ts
+++ packages/server/build_npm.ts
@@ -101,8 +101,11 @@ await build({
     },
     // Mapping for '../../types/src/index.ts' in deps.ts
     '../types/src/index.ts': {
-      name: '@simplewebauthn/types',
-      version: `^${typesPackageJSON.version}`,
+      // TODO: Update this to point to @simplewebauthn/types after publishing it to NPM
+      name: '@simplewebauthn/typescript-types',
+      version: '^8.3.4',
+      // name: '@simplewebauthn/types',
+      // version: `^${typesPackageJSON.version}`,
     },
   },
   // TypeScript tsconfig.json config
