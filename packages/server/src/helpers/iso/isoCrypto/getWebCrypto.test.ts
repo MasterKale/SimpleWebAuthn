4cdeb5ef9c3617e64647ac72b19500fbbfd19b7a
Matthew Miller <matthew@millerti.me>
2024-02-26 16:39:22 -0800

    Update getWebCrypto for Node 20 globalThis.crypto

--- packages/server/src/helpers/iso/isoCrypto/getWebCrypto.test.ts
+++ packages/server/src/helpers/iso/isoCrypto/getWebCrypto.test.ts
@@ -23,101 +23,6 @@ Deno.test('should return globalThis.crypto when present', async () => {
   mockGlobalThisCrypto.restore();
 });
 
-Deno.test('should return node:crypto.webcrypto when globalThis.crypto is missing', async () => {
-  // Clear whatever version of crypto might have been set
-  _getWebCryptoInternals.setCachedCrypto(undefined);
-
-  // Pretend globalThis.crypto doesn't exist
-  const mockGlobalThisCrypto = stub(
-    _getWebCryptoInternals,
-    'stubThisGlobalThisCrypto',
-    // @ts-ignore: globalThis.crypto
-    returnsNext([undefined]),
-  );
-
-  // Mock out just enough of the 'node:crypto' module
-  const fakeNodeCrypto = { webcrypto: {} };
-  const mockImportNodeCrypto = stub(
-    _getWebCryptoInternals,
-    'stubThisImportNodeCrypto',
-    // @ts-ignore: node:crypto
-    returnsNext([Promise.resolve(fakeNodeCrypto)]),
-  );
-
-  const returnedCrypto = await getWebCrypto();
-
-  assertEquals(returnedCrypto, fakeNodeCrypto.webcrypto);
-
-  mockGlobalThisCrypto.restore();
-  mockImportNodeCrypto.restore();
-});
-
-Deno.test(
-  'should return globalThis.crypto when present, while node:crypto.webcrypto is present',
-  async () => {
-    // Clear whatever version of crypto might have been set
-    _getWebCryptoInternals.setCachedCrypto(undefined);
-
-    // Pretend globalThis.crypto exists
-    const fakeGlobalThisCrypto = {};
-    const mockGlobalThisCrypto = stub(
-      _getWebCryptoInternals,
-      'stubThisGlobalThisCrypto',
-      // @ts-ignore: globalThis.crypto
-      returnsNext([fakeGlobalThisCrypto]),
-    );
-
-    // Mock out just enough of the 'node:crypto' module, but like we're in Node v14
-    const fakeNodeCrypto = { webcrypto: {} };
-    const mockImportNodeCrypto = stub(
-      _getWebCryptoInternals,
-      'stubThisImportNodeCrypto',
-      // @ts-ignore: node:crypto
-      returnsNext([Promise.resolve(fakeNodeCrypto)]),
-    );
-
-    const returnedCrypto = await getWebCrypto();
-
-    assertEquals(returnedCrypto, fakeGlobalThisCrypto);
-
-    mockGlobalThisCrypto.restore();
-    mockImportNodeCrypto.restore();
-  },
-);
-
-Deno.test(
-  'should return globalThis.crypto when present, while node:crypto is present but missing webcrypto',
-  async () => {
-    // Clear whatever version of crypto might have been set
-    _getWebCryptoInternals.setCachedCrypto(undefined);
-
-    // Pretend globalThis.crypto exists
-    const fakeGlobalThisCrypto = {};
-    const mockGlobalThisCrypto = stub(
-      _getWebCryptoInternals,
-      'stubThisGlobalThisCrypto',
-      // @ts-ignore: globalThis.crypto
-      returnsNext([fakeGlobalThisCrypto]),
-    );
-
-    // Mock out just enough of the 'node:crypto' module, but like we're in Node v14
-    const fakeNodeCrypto = { webcrypto: undefined };
-    const mockImportNodeCrypto = stub(
-      _getWebCryptoInternals,
-      'stubThisImportNodeCrypto',
-      // @ts-ignore: node:crypto
-      returnsNext([Promise.resolve(fakeNodeCrypto)]),
-    );
-
-    const returnedCrypto = await getWebCrypto();
-
-    assertEquals(returnedCrypto, fakeGlobalThisCrypto);
-
-    mockGlobalThisCrypto.restore();
-    mockImportNodeCrypto.restore();
-  },
-);
-
 Deno.test('should raise MissingWebCrypto error when nothing is available', async () => {
   // Clear whatever version of crypto might have been set
   _getWebCryptoInternals.setCachedCrypto(undefined);
@@ -130,14 +35,6 @@ Deno.test('should raise MissingWebCrypto error when nothing is available', async
     returnsNext([undefined]),
   );
 
-  // Pretend node:crypto doesn't exist
-  const mockImportNodeCrypto = stub(
-    _getWebCryptoInternals,
-    'stubThisImportNodeCrypto',
-    // @ts-ignore: node:crypto
-    returnsNext([Promise.resolve({ webcrypto: undefined })]),
-  );
-
   await assertRejects(
     () => getWebCrypto(),
     MissingWebCrypto,
@@ -145,5 +42,4 @@ Deno.test('should raise MissingWebCrypto error when nothing is available', async
   );
 
   mockGlobalThisCrypto.restore();
-  mockImportNodeCrypto.restore();
 });
