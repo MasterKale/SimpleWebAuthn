02190d8265c65ee30f2e937eda102d6325945f7f
Clara Fitzgerald <claraisabelfitzgerald@gmail.com>
2024-04-02 20:14:58 +0100

    fix(browser): handle undefined PublicKeyCredential in browserSupportsWebAuthnAutofill helper

--- packages/browser/src/helpers/browserSupportsWebAuthnAutofill.ts
+++ packages/browser/src/helpers/browserSupportsWebAuthnAutofill.ts
@@ -1,10 +1,16 @@
-import { PublicKeyCredentialFuture } from '@simplewebauthn/types';
+import { PublicKeyCredentialFuture } from "@simplewebauthn/types";
+
+import { browserSupportsWebAuthn } from "./browserSupportsWebAuthn";
 
 /**
  * Determine if the browser supports conditional UI, so that WebAuthn credentials can
  * be shown to the user in the browser's typical password autofill popup.
  */
 export function browserSupportsWebAuthnAutofill(): Promise<boolean> {
+  if (!browserSupportsWebAuthn()) {
+    return new Promise((resolve) => resolve(false));
+  }
+
   /**
    * I don't like the `as unknown` here but there's a `declare var PublicKeyCredential` in
    * TS' DOM lib that's making it difficult for me to just go `as PublicKeyCredentialFuture` as I
