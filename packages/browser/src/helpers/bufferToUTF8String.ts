6af30185938addf9a12c6076dd460f5f0c5146f9
Matthew Miller <matthew@millerti.me>
2024-04-11 21:42:59 -0700

    Remove UTF-8 string helpers from browser

--- packages/browser/src/helpers/bufferToUTF8String.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A helper method to convert an arbitrary ArrayBuffer, returned from an authenticator, to a UTF-8
- * string.
- */
-export function bufferToUTF8String(value: ArrayBuffer): string {
-  return new TextDecoder('utf-8').decode(value);
-}
