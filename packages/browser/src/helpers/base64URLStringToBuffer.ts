424792e0d2c713ad9ec7b9962db472749faf5aad
Islam <2553451+qalandarov@users.noreply.github.com>
2024-05-05 11:18:13 +0100

    Simplify base64URLStringToBuffer padding

--- packages/browser/src/helpers/base64URLStringToBuffer.ts
+++ packages/browser/src/helpers/base64URLStringToBuffer.ts
@@ -7,19 +7,15 @@
  */
 export function base64URLStringToBuffer(base64URLString: string): ArrayBuffer {
   // Convert from Base64URL to Base64
-  const base64 = base64URLString.replace(/-/g, '+').replace(/_/g, '/');
-  /**
-   * Pad with '=' until it's a multiple of four
-   * (4 - (85 % 4 = 1) = 3) % 4 = 3 padding
-   * (4 - (86 % 4 = 2) = 2) % 4 = 2 padding
-   * (4 - (87 % 4 = 3) = 1) % 4 = 1 padding
-   * (4 - (88 % 4 = 0) = 4) % 4 = 0 padding
-   */
-  const padLength = (4 - (base64.length % 4)) % 4;
-  const padded = base64.padEnd(base64.length + padLength, '=');
+  let base64 = base64URLString.replace(/-/g, '+').replace(/_/g, '/');
+
+  // Pad with '=' until it's a multiple of four
+  while (base64.length % 4 != 0) {
+    base64 += "=";
+  }
 
   // Convert to a binary string
-  const binary = atob(padded);
+  const binary = atob(base64);
 
   // Convert binary string to buffer
   const buffer = new ArrayBuffer(binary.length);
