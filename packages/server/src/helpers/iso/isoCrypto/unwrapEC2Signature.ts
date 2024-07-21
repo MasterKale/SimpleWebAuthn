215dc5d368e6d31dd66873c8002b1880b80c2dbe
Nicholas Rodrigues Lordello <n@lordello.net>
2024-07-21 09:20:02 +0200

    Implement PR Review suggestions
    Co-authored-by: Matthew Miller <matthew@millerti.me>

--- packages/server/src/helpers/iso/isoCrypto/unwrapEC2Signature.ts
+++ packages/server/src/helpers/iso/isoCrypto/unwrapEC2Signature.ts
@@ -9,19 +9,25 @@ import { isoUint8Array } from '../index.ts';
  */
 export function unwrapEC2Signature(signature: Uint8Array, crv: COSECRV): Uint8Array {
   const parsedSignature = AsnParser.parse(signature, ECDSASigValue);
-  const n = getSignatureComponentLength(crv);
+  const rBytes = new Uint8Array(parsedSignature.r);
+  const sBytes = new Uint8Array(parsedSignature.s);
 
-  const rBytes = toNormalizedBytes(parsedSignature.r, n);
-  const sBytes = toNormalizedBytes(parsedSignature.s, n);
+  const componentLength = getSignatureComponentLength(crv);
+  const rNormalizedBytes = toNormalizedBytes(rBytes, componentLength);
+  const sNormalizedBytes = toNormalizedBytes(sBytes, componentLength);
 
-  const finalSignature = isoUint8Array.concat([rBytes, sBytes]);
+  const finalSignature = isoUint8Array.concat([
+    rNormalizedBytes,
+    sNormalizedBytes,
+  ]);
 
   return finalSignature;
 }
 
 /**
- * ECDSA signatures with in the subtle crypto API expect signatures with `r` and `s` values
- * encoded to a specific length depending on the order of the curve.
+ * The SubtleCrypto Web Crypto API expects ECDSA signatures with `r` and `s` values to be encoded
+ * to a specific length depending on the order of the curve. This function returns the expected
+ * byte-length for each of the `r` and `s` signature components.
  *
  * See <https://www.w3.org/TR/WebCryptoAPI/#ecdsa-operations>
  */
@@ -42,24 +48,28 @@ function getSignatureComponentLength(crv: COSECRV): number {
  * Converts the ASN.1 integer representation to bytes of a specific length `n`.
  *
  * DER encodes integers as big-endian byte arrays, with as small as possible representation and
- * require leading `0` bytes to disambiguate between negative and positive numbers. This means
- * that `r` and `s` can potentially not be the expected length `n` that is needed by the WebCrypto
- * subtle API: if there it leading `0`s it can be shorter than expected, and if it has a leading
- * `1` bit, it can be one byte longer.
+ * requires a leading `0` byte to disambiguate between negative and positive numbers. This means
+ * that `r` and `s` can potentially not be the expected byte-length that is needed by the
+ * SubtleCrypto Web Crypto API: if there are leading `0`s it can be shorter than expected, and if
+ * it has a leading `1` bit, it can be one byte longer.
  *
  * See <https://www.itu.int/rec/T-REC-X.690-202102-I/en>
  * See <https://www.w3.org/TR/WebCryptoAPI/#ecdsa-operations>
  */
-function toNormalizedBytes(i: ArrayBuffer, n: number): Uint8Array {
-  const iBytes = new Uint8Array(i);
-
-  const normalizedBytes = new Uint8Array(n);
-  if (iBytes.length <= n) {
-    normalizedBytes.set(iBytes, n - iBytes.length);
-  } else if (iBytes.length === n + 1 && iBytes[0] === 0 && (iBytes[1] & 0x80) === 0x80) {
-    normalizedBytes.set(iBytes.slice(1));
+function toNormalizedBytes(bytes: Uint8Array, componentLength: number): Uint8Array {
+  let normalizedBytes;
+  if (bytes.length < componentLength) {
+    // In case the bytes are shorter than expected, we need to pad it with leading `0`s.
+    normalizedBytes = new Uint8Array(componentLength);
+    normalizedBytes.set(bytes, componentLength - bytes.length);
+  } else if (bytes.length === componentLength) {
+    normalizedBytes = bytes;
+  } else if (bytes.length === componentLength + 1 && bytes[0] === 0 && (bytes[1] & 0x80) === 0x80) {
+    // The bytes contain a leading `0` to encode that the integer is positive. This leading `0`
+    // needs to be removed for compatibility with the SubtleCrypto Web Crypto API.
+    normalizedBytes = bytes.subarray(1);
   } else {
-    throw new Error("invalid signature component length");
+    throw new Error(`invalid signature component length ${bytes.length} (expected ${componentLength})`);
   }
 
   return normalizedBytes;
