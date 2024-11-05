250650812cf53e9e1fae2db7950669aa3eed64dd
Matthew Miller <matthew@millerti.me>
2024-11-05 15:23:56 -0800

    Make it possible to stub browserSupportsWebAuthn

--- packages/browser/src/helpers/browserSupportsWebAuthn.ts
+++ packages/browser/src/helpers/browserSupportsWebAuthn.ts
@@ -2,8 +2,13 @@
  * Determine if the browser is capable of Webauthn
  */
 export function browserSupportsWebAuthn(): boolean {
-  return (
+  return _browserSupportsWebAuthnInternals.stubThis((
     globalThis?.PublicKeyCredential !== undefined &&
     typeof globalThis.PublicKeyCredential === 'function'
-  );
+  ));
 }
+
+// Make it possible to stub the return value during testing
+export const _browserSupportsWebAuthnInternals = {
+  stubThis: (value: boolean) => value,
+};
