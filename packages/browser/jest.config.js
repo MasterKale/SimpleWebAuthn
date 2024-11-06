c6f28c3522e58e2b4c2dbc8b8789e0a50189da73
Matthew Miller <matthew@millerti.me>
2024-11-05 22:26:03 -0800

    Delete Jest configs

--- packages/browser/jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  collectCoverageFrom: ['<rootDir>/src/**/*.{js,ts}'],
-  coverageDirectory: 'coverage',
-  testEnvironment: '<rootDir>/jest-environment.js',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-};
