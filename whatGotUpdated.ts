e3f05b0f2e22880c803d3f5ddc33652f81a96ea9
Matthew Miller <matthew@millerti.me>
2024-11-23 14:06:41 -0800

    Finish initial versioning script

--- whatGotUpdated.ts
+++ whatGotUpdated.ts
@@ -3,10 +3,113 @@
  * git tag version. These packages will become candidates for version bumps.
  */
 
-// TODO: Check which files have been updated since the last git tag
+// Grab workspace folders from root deno.json
+import rootDenoJSON from './deno.json' with { type: 'json' };
 
-// TODO: Determine which of the three packages have been changed
+// Trim relative path specifier so we end up with ["packages/browser", ...]
+const workspaceDirs = rootDenoJSON.workspace.map((path) => path.substring(2));
 
-// TODO: Read current versions from corresponding deno.jsonc files
+// Check which files have been updated since the last git tag
+console.log('Getting latest version tag...');
+const latestTag = await getLatestVersionTag();
 
-// Output package names and their current versions to consider incrementing
+console.log(`Getting files changed since tag ${latestTag}...`);
+const changedFiles = await getChangedFilesSinceTag(latestTag);
+
+// Determine which of the three packages have been changed
+console.log('Checking which workspace packages have changed...');
+const changedPackages = getChangedWorkspacePackages(changedFiles, workspaceDirs);
+
+/**
+ * Determine final output
+ */
+if (changedPackages.length < 1) {
+  console.log('✅ No workspace packages have been updated since the last version tag');
+} else {
+  console.log('The following workspace packages need new versions published:');
+
+  for (const matched of changedPackages) {
+    // Read current versions from corresponding deno.json files
+    const packageDenoJSONPath = `./${matched}/deno.json`;
+    const { default: packageDenoJSON } = await import(packageDenoJSONPath, {
+      with: { type: 'json' },
+    });
+
+    // Output package names and their current versions to consider incrementing
+    const formattedMatched = `@simplewebauthn/${matched.split('/')[1]}`;
+    console.log(
+      `📦 ${formattedMatched} (current version: ${packageDenoJSON.version} @ ${packageDenoJSONPath})`,
+    );
+  }
+}
+
+/**
+ * Grab the latest version tag from Git
+ */
+async function getLatestVersionTag(): Promise<string> {
+  const command = new Deno.Command('git', {
+    args: [
+      'describe',
+      '--tags',
+      '--abbrev=0',
+    ],
+  });
+
+  const { code, stdout, stderr } = await command.output();
+
+  if (code !== 0) {
+    throw new Error(new TextDecoder().decode(stderr));
+  }
+
+  const output = new TextDecoder().decode(stdout);
+  const toReturn = output.trim();
+
+  // Verify we got something back that looks like a typical version tag
+  if (!toReturn.match(/v\d+.\d+.\d+/i)) {
+    throw new Error(`Tag "${toReturn}" was not the expected version tag format of vXX.XX.XX`);
+  }
+
+  return toReturn;
+}
+
+/**
+ * Get the list of files changed since the provided version tag
+ */
+async function getChangedFilesSinceTag(tagName: string): Promise<string[]> {
+  const command = new Deno.Command('git', {
+    args: [
+      'diff',
+      '--name-only',
+      `${tagName}..HEAD`,
+    ],
+  });
+
+  const { code, stdout, stderr } = await command.output();
+
+  if (code !== 0) {
+    throw new Error(new TextDecoder().decode(stderr));
+  }
+
+  const output = new TextDecoder().decode(stdout);
+
+  const toReturn = output.split('\n');
+
+  return toReturn;
+}
+
+/**
+ * Look through the list of files to figure out which workspace packages have changed
+ */
+function getChangedWorkspacePackages(changedFiles: string[], workspaceDirs: string[]): string[] {
+  const matchedPackages = new Set<string>();
+
+  for (const file of changedFiles) {
+    for (const workspace of workspaceDirs) {
+      if (file.startsWith(workspace)) {
+        matchedPackages.add(workspace);
+      }
+    }
+  }
+
+  return matchedPackages.values().toArray();
+}
