370df5a08b791ba82b2b139ce3d96e85b256c131
Matthew Miller <matthew@millerti.me>
2024-11-23 00:18:29 -0800

    Outline versioning script steps

---
+++ whatGotUpdated.ts
@@ -0,0 +1,12 @@
+/**
+ * A script to determine which workspace packages contain code that's been updated since the last
+ * git tag version. These packages will become candidates for version bumps.
+ */
+
+// TODO: Check which files have been updated since the last git tag
+
+// TODO: Determine which of the three packages have been changed
+
+// TODO: Read current versions from corresponding deno.jsonc files
+
+// Output package names and their current versions to consider incrementing
