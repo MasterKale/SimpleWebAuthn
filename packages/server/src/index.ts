4217841cc7731fd1a5a95abbf7c2020790fd9106
Matthew Miller <matthew@millerti.me>
2024-12-08 12:37:45 -0800

    Simplify browser and server exports

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,40 +1,8 @@
-import { generateRegistrationOptions } from './registration/generateRegistrationOptions.ts';
-import { verifyRegistrationResponse } from './registration/verifyRegistrationResponse.ts';
-import { generateAuthenticationOptions } from './authentication/generateAuthenticationOptions.ts';
-import { verifyAuthenticationResponse } from './authentication/verifyAuthenticationResponse.ts';
-import { MetadataService } from './services/metadataService.ts';
-import { SettingsService } from './services/settingsService.ts';
-
-export {
-  generateAuthenticationOptions,
-  generateRegistrationOptions,
-  MetadataService,
-  SettingsService,
-  verifyAuthenticationResponse,
-  verifyRegistrationResponse,
-};
-
-import type { GenerateRegistrationOptionsOpts } from './registration/generateRegistrationOptions.ts';
-import type { GenerateAuthenticationOptionsOpts } from './authentication/generateAuthenticationOptions.ts';
-import type { MetadataStatement } from './metadata/mdsTypes.ts';
-import type {
-  VerifiedRegistrationResponse,
-  VerifyRegistrationResponseOpts,
-} from './registration/verifyRegistrationResponse.ts';
-import type {
-  VerifiedAuthenticationResponse,
-  VerifyAuthenticationResponseOpts,
-} from './authentication/verifyAuthenticationResponse.ts';
-
-export type {
-  GenerateAuthenticationOptionsOpts,
-  GenerateRegistrationOptionsOpts,
-  MetadataStatement,
-  VerifiedAuthenticationResponse,
-  VerifiedRegistrationResponse,
-  VerifyAuthenticationResponseOpts,
-  VerifyRegistrationResponseOpts,
-};
-
-export * from './types/index.ts';
+export * from './registration/generateRegistrationOptions.ts';
+export * from './registration/verifyRegistrationResponse.ts';
+export * from './authentication/generateAuthenticationOptions.ts';
+export * from './authentication/verifyAuthenticationResponse.ts';
+export * from './services/metadataService.ts';
+export * from './services/settingsService.ts';
 export * from './metadata/mdsTypes.ts';
+export * from './types/index.ts';
