fdbb8809faa60cc37d6f707ba37124695deb8603
Matthew Miller <matthew@millerti.me>
2025-07-05 10:53:08 -0700

    Codegen updated types

--- packages/browser/src/types/index.ts
+++ packages/browser/src/types/index.ts
@@ -46,6 +46,7 @@ export type {
   PublicKeyCredentialRpEntity,
   PublicKeyCredentialType,
   PublicKeyCredentialUserEntity,
+  ResidentKeyRequirement,
   UserVerificationRequirement,
 } from './dom.ts';
 
