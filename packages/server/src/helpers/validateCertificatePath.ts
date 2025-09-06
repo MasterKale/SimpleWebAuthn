e4b7859256e30c8db1aa4d8b33791f563f91bfda
Matthew Miller <matthew@millerti.me>
2025-09-06 12:49:49 -0700

    Refactor cert chain validation

--- packages/server/src/helpers/validateCertificatePath.ts
+++ packages/server/src/helpers/validateCertificatePath.ts
@@ -1,13 +1,6 @@
-import { AsnSerializer } from '@peculiar/asn1-schema';
-import type { Certificate } from '@peculiar/asn1-x509';
-import * as x509 from '@peculiar/x509';
+import { X509Certificate } from '@peculiar/x509';
 
 import { isCertRevoked } from './isCertRevoked.ts';
-import { verifySignature } from './verifySignature.ts';
-import { mapX509SignatureAlgToCOSEAlg } from './mapX509SignatureAlgToCOSEAlg.ts';
-import { type CertificateInfo, getCertificateInfo } from './getCertificateInfo.ts';
-import { convertPEMToBytes } from './convertPEMToBytes.ts';
-import { convertCertBufferToPEM } from './convertCertBufferToPEM.ts';
 import { getWebCrypto } from './iso/isoCrypto/getWebCrypto.ts';
 
 /**
@@ -24,22 +17,112 @@ export async function validateCertificatePath(
     return true;
   }
 
-  let invalidSubjectAndIssuerError = false;
-  let certificateNotYetValidOrExpiredErrorMessage = undefined;
-  for (const anchorPEM of trustAnchorsPEM) {
+  const WebCrypto = await getWebCrypto();
+
+  // Prepare to work with x5c certs
+  const x5cCertsParsed = x5cCertsPEM.map((certPEM) => new X509Certificate(certPEM));
+
+  // Check for any expired or temporally invalid certs in x5c
+  for (let i = 0; i < x5cCertsParsed.length; i++) {
+    const cert = x5cCertsParsed[i];
+    const certPEM = x5cCertsPEM[i];
+
     try {
-      const certsWithTrustAnchor = x5cCertsPEM.concat([anchorPEM]);
-      await _validatePath(certsWithTrustAnchor);
+      await assertCertNotRevoked(cert);
+    } catch (_err) {
+      throw new Error(`Found revoked certificate in x5c:\n${certPEM}`);
+    }
+
+    try {
+      assertCertIsWithinValidTimeWindow(cert.notBefore, cert.notAfter);
+    } catch (_err) {
+      throw new Error(`Found certificate out of validity period in x5c:\n${certPEM}`);
+    }
+  }
+
+  // Prepare to work with trust anchor certs
+  const trustAnchorsParsed = trustAnchorsPEM.map((certPEM) => {
+    try {
+      return new X509Certificate(certPEM);
+    } catch (err) {
+      const _err = err as Error;
+      throw new Error(`Could not parse trust anchor certificate:\n${certPEM}`, { cause: _err });
+    }
+  });
+
+  // Filter out any expired or temporally invalid trust anchors certs
+  const validTrustAnchors: X509Certificate[] = [];
+  for (let i = 0; i < trustAnchorsParsed.length; i++) {
+    const cert = trustAnchorsParsed[i];
+
+    try {
+      await assertCertNotRevoked(cert);
+    } catch (_err) {
+      //
+      continue;
+    }
+
+    try {
+      assertCertIsWithinValidTimeWindow(cert.notBefore, cert.notAfter);
+    } catch (_err) {
+      continue;
+    }
+
+    validTrustAnchors.push(cert);
+  }
+
+  if (validTrustAnchors.length === 0) {
+    throw new Error('No specified trust anchor was valid for verifying x5c');
+  }
+
+  // Try to verify x5c with each trust anchor
+  let invalidSubjectAndIssuerError = false;
+  for (const anchor of trustAnchorsParsed) {
+    try {
+      const x5cWithTrustAnchor = x5cCertsParsed.concat([anchor]);
+
+      if (new Set(x5cWithTrustAnchor).size !== x5cWithTrustAnchor.length) {
+        throw new Error('Invalid certificate path: found duplicate certificates');
+      }
+
+      // Check signatures, and notBefore and notAfter
+      for (let i = 0; i < x5cWithTrustAnchor.length - 1; i++) {
+        const subject = x5cWithTrustAnchor[i];
+        const issuer = x5cWithTrustAnchor[i + 1];
+
+        // Leaf or intermediate cert, make sure the next cert in the chain signed it
+        const issuerSignedSubject = await subject.verify(
+          { publicKey: issuer.publicKey, signatureOnly: true },
+          WebCrypto,
+        );
+
+        if (!issuerSignedSubject) {
+          throw new InvalidSubjectAndIssuer();
+        }
+
+        if (issuer.subject === issuer.issuer) {
+          // Root cert detected, make sure it signed itself
+          const issuerSignedIssuer = await issuer.verify(
+            { publicKey: issuer.publicKey, signatureOnly: true },
+            WebCrypto,
+          );
+
+          if (!issuerSignedIssuer) {
+            throw new InvalidSubjectAndIssuer();
+          }
+
+          // Don't process anything else after a root cert
+          break;
+        }
+      }
+
       // If we successfully validated a path then there's no need to continue. Reset any existing
       // errors that were thrown by earlier trust anchors
       invalidSubjectAndIssuerError = false;
-      certificateNotYetValidOrExpiredErrorMessage = undefined;
       break;
     } catch (err) {
       if (err instanceof InvalidSubjectAndIssuer) {
         invalidSubjectAndIssuerError = true;
-      } else if (err instanceof CertificateNotYetValidOrExpired) {
-        certificateNotYetValidOrExpiredErrorMessage = err.message;
       } else {
         throw err;
       }
@@ -49,77 +132,6 @@ export async function validateCertificatePath(
   // We tried multiple trust anchors and none of them worked
   if (invalidSubjectAndIssuerError) {
     throw new InvalidSubjectAndIssuer();
-  } else if (certificateNotYetValidOrExpiredErrorMessage) {
-    throw new CertificateNotYetValidOrExpired(
-      certificateNotYetValidOrExpiredErrorMessage,
-    );
-  }
-
-  return true;
-}
-
-/**
- * @param x5cCerts X.509 `x5c` certs in PEM string format
- * @param anchorCert X.509 trust anchor cert in PEM string format
- */
-async function _validatePath(x5cCertsWithTrustAnchorPEM: string[]): Promise<boolean> {
-  if (new Set(x5cCertsWithTrustAnchorPEM).size !== x5cCertsWithTrustAnchorPEM.length) {
-    throw new Error('Invalid certificate path: found duplicate certificates');
-  }
-
-  // TODO: Build cert chain (includes signature verification)
-  const leafCertPEM = x5cCertsWithTrustAnchorPEM[0];
-  const leafCertParsed = new x509.X509Certificate(leafCertPEM);
-  const intermediateAndAnchorCertsPEM = x5cCertsWithTrustAnchorPEM.slice(1);
-  const intermediateAndAnchorCertsParsed = intermediateAndAnchorCertsPEM.map((certPEM) =>
-    new x509.X509Certificate(certPEM)
-  );
-
-  const crypto = await getWebCrypto();
-  const builder = new x509.X509ChainBuilder({ certificates: intermediateAndAnchorCertsParsed });
-  const chain = await builder.build(leafCertParsed, crypto);
-
-  for (const cert of chain) {
-    // TODO: Check certs are all within valid time window
-    assertCertIsWithinValidTimeWindow(
-      cert.notBefore,
-      cert.notAfter,
-      convertCertBufferToPEM(new Uint8Array(cert.rawData)),
-    );
-
-    // TODO: Check certs are not revoked
-    const extCRL = cert.getExtensions(x509.CRLDistributionPointsExtension);
-    console.log(extCRL);
-  }
-
-  // Make sure no certs are revoked, and all are within their time validity window
-  // for (const certificatePEM of x5cCertsWithTrustAnchorPEM) {
-  //   const certInfo = getCertificateInfo(convertPEMToBytes(certificatePEM));
-  //   await assertCertNotRevoked(certInfo.parsedCertificate);
-  //   assertCertIsWithinValidTimeWindow(certInfo.notBefore, certInfo.notAfter, certificatePEM);
-  // }
-
-  // Make sure each x5c cert is issued by the next certificate in the chain
-  for (let i = 0; i < (x5cCertsWithTrustAnchorPEM.length - 1); i += 1) {
-    const subjectPem = x5cCertsWithTrustAnchorPEM[i];
-    const issuerPem = x5cCertsWithTrustAnchorPEM[i + 1];
-
-    const subjectInfo = getCertificateInfo(convertPEMToBytes(subjectPem));
-    const issuerInfo = getCertificateInfo(convertPEMToBytes(issuerPem));
-
-    // Make sure subject issuer is issuer subject
-    if (subjectInfo.issuer.combined !== issuerInfo.subject.combined) {
-      throw new InvalidSubjectAndIssuer();
-    }
-
-    const issuerCertIsRootCert = issuerInfo.issuer.combined === issuerInfo.subject.combined;
-
-    await assertSubjectIsSignedByIssuer(subjectInfo.parsedCertificate, issuerPem);
-
-    // Perform one final check if the issuer cert is also a root certificate
-    if (issuerCertIsRootCert) {
-      await assertSubjectIsSignedByIssuer(issuerInfo.parsedCertificate, issuerPem);
-    }
   }
 
   return true;
@@ -128,58 +140,23 @@ async function _validatePath(x5cCertsWithTrustAnchorPEM: string[]): Promise<bool
 /**
  * Check if the certificate is revoked or not. If it is, raise an error
  */
-async function assertCertNotRevoked(certificate: Certificate): Promise<void> {
+async function assertCertNotRevoked(certificate: X509Certificate): Promise<void> {
   // Check for certificate revocation
   const subjectCertRevoked = await isCertRevoked(certificate);
 
   if (subjectCertRevoked) {
-    throw new Error(`Found revoked certificate in certificate path`);
+    throw new Error('Found revoked certificate in certificate path');
   }
 }
 
 /**
  * Require the cert to be within its notBefore and notAfter time window
- *
- * @param certInfo Parsed cert information
- * @param certPEM PEM-formatted certificate, for error reporting
  */
-function assertCertIsWithinValidTimeWindow(
-  certNotBefore: Date,
-  certNotAfter: Date,
-  certPEM: string,
-): void {
+function assertCertIsWithinValidTimeWindow(certNotBefore: Date, certNotAfter: Date): void {
+  // console.log({ notBefore: certNotBefore, notAfter: certNotAfter });
   const now = new Date(Date.now());
   if (certNotBefore > now || certNotAfter < now) {
-    throw new CertificateNotYetValidOrExpired(
-      `Certificate is not yet valid or expired: ${certPEM}`,
-    );
-  }
-}
-
-/**
- * Ensure that the subject cert has been signed by the next cert in the chain
- */
-async function assertSubjectIsSignedByIssuer(
-  subjectCert: Certificate,
-  issuerPEM: string,
-): Promise<void> {
-  // Verify the subject certificate's signature with the issuer cert's public key
-  const data = AsnSerializer.serialize(subjectCert.tbsCertificate);
-  const signature = subjectCert.signatureValue;
-  const signatureAlgorithm = mapX509SignatureAlgToCOSEAlg(
-    subjectCert.signatureAlgorithm.algorithm,
-  );
-  const issuerCertBytes = convertPEMToBytes(issuerPEM);
-
-  const verified = await verifySignature({
-    data: new Uint8Array(data),
-    signature: new Uint8Array(signature),
-    x509Certificate: issuerCertBytes,
-    hashAlgorithm: signatureAlgorithm,
-  });
-
-  if (!verified) {
-    throw new InvalidSubjectSignatureForIssuer();
+    throw new Error('Certificate is not yet valid or expired');
   }
 }
 
@@ -189,20 +166,5 @@ class InvalidSubjectAndIssuer extends Error {
     const message = 'Subject issuer did not match issuer subject';
     super(message);
     this.name = 'InvalidSubjectAndIssuer';
-  }
-}
-
-class InvalidSubjectSignatureForIssuer extends Error {
-  constructor() {
-    const message = 'Subject signature was invalid for issuer';
-    super(message);
-    this.name = 'InvalidSubjectSignatureForIssuer';
-  }
-}
-
-class CertificateNotYetValidOrExpired extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'CertificateNotYetValidOrExpired';
   }
 }
