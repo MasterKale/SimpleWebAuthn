4ccada71ecdc894890441e87d64f814b2ca3a5c5
Matthew Miller <matthew@millerti.me>
2026-03-02 21:59:33 -0800

    Add some tests

---
+++ packages/browser/src/helpers/isValidDomain.test.ts
@@ -0,0 +1,26 @@
+import { assert, assertFalse } from '@std/assert';
+import { isValidDomain } from './isValidDomain.ts';
+
+Deno.test('should handle localhost', () => {
+  assert(isValidDomain('localhost'));
+});
+
+Deno.test('should handle standard ASCII domains and w/subdomains', () => {
+  assert(isValidDomain('example.com'));
+  assert(isValidDomain('my-site.io'));
+  assert(isValidDomain('sub.example.co.uk'));
+  assertFalse(isValidDomain('notadomain'));
+  assertFalse(isValidDomain(''));
+});
+
+Deno.test('should handle punycode domains', () => {
+  // Punycode label with ascii domain
+  assert(isValidDomain('xn--5lwo46cp2i.co.jp'));
+  assert(isValidDomain('xn--5lwo46cp2i.jp'));
+  // Punycode label with punycode domain
+  assert(isValidDomain('xn--80akjhbed8ahk.xn--p1ai'));
+  // ASCII subdomain
+  assert(isValidDomain('login.xn--5lwo46cp2i.co.jp'));
+  // Punycode subdomain
+  assert(isValidDomain('xn--sub.xn--5lwo46cp2i.co.jp'));
+});
