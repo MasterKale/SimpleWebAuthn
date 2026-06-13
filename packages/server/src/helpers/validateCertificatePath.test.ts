6ee964484ad9bb9365c45c95b32e0065db45ed96
Matthew Miller <matthew@millerti.me>
2026-06-13 04:38:30 -0700

    Add new tests

--- packages/server/src/helpers/validateCertificatePath.test.ts
+++ packages/server/src/helpers/validateCertificatePath.test.ts
@@ -1,4 +1,4 @@
-import { assertRejects } from '@std/assert';
+import { assert, assertRejects } from '@std/assert';
 import { FakeTime } from '@std/testing/time';
 
 import { validateCertificatePath } from './validateCertificatePath.ts';
@@ -53,5 +53,80 @@ Deno.test('should reject x5c containing self-signed root certificate', async () 
       ),
     Error,
     'x5c could not be chained to any specified trust anchor',
+  );
+});
+
+Deno.test('should validate valid certificate chain', async () => {
+  using _fakedNow = new FakeTime(new Date('2026-06-08'));
+
+  const notBefore = new Date('2026-06-07');
+  const notAfter = new Date('2026-06-09');
+
+  const rootCert = await generateRootCert({ notBefore, notAfter });
+  const leafCert = await generateLeafCert({
+    notBefore,
+    notAfter,
+    chainsToCertificate: rootCert.certificate,
+    chainsToPrivateKey: rootCert.keys.privateKey,
+  });
+
+  const validated = await validateCertificatePath(
+    [leafCert.toString()],
+    [rootCert.certificate.toString()],
+  );
+
+  assert(validated);
+});
+
+Deno.test('should raise on not-yet-valid leaf certificate', async () => {
+  using _fakedNow = new FakeTime(new Date('2026-06-08'));
+
+  const notBefore = new Date('2026-06-07');
+  const notAfter = new Date('2026-06-09');
+
+  const rootCert = await generateRootCert({ notBefore, notAfter });
+  const leafCert = await generateLeafCert({
+    notBefore: new Date('2026-06-09'), // <-- later than _fakedNow
+    notAfter,
+    chainsToCertificate: rootCert.certificate,
+    chainsToPrivateKey: rootCert.keys.privateKey,
+  });
+
+  await assertRejects(
+    () =>
+      validateCertificatePath(
+        [leafCert.toString()],
+        [rootCert.certificate.toString()],
+      ),
+    Error,
+    'certificate out of validity period in x5c',
+  );
+});
+
+Deno.test('should raise on not-yet-valid trust anchor certificate', async () => {
+  using _fakedNow = new FakeTime(new Date('2026-06-08'));
+
+  const notBefore = new Date('2026-06-07');
+  const notAfter = new Date('2026-06-09');
+
+  const rootCert = await generateRootCert({
+    notBefore: new Date('2026-06-09'), // <-- later than _fakedNow
+    notAfter,
+  });
+  const leafCert = await generateLeafCert({
+    notBefore,
+    notAfter,
+    chainsToCertificate: rootCert.certificate,
+    chainsToPrivateKey: rootCert.keys.privateKey,
+  });
+
+  await assertRejects(
+    () =>
+      validateCertificatePath(
+        [leafCert.toString()],
+        [rootCert.certificate.toString()],
+      ),
+    Error,
+    'No specified trust anchor was valid',
   );
 });
