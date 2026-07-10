b0b9a07682c5725866cc3d469e090dbad0120f49
Matthew Miller <matthew@millerti.me>
2026-07-09 22:28:43 -0700

    Identify WebAuthn signal errors

--- packages/browser/src/helpers/identifySignalError.ts
+++ packages/browser/src/helpers/identifySignalError.ts
@@ -1,4 +1,81 @@
+import { isValidDomain } from './isValidDomain.ts';
+import { WebAuthnError } from './webAuthnError.ts';
+import type {
+  SignalAllAcceptedCredentialsOpts,
+  SignalCurrentUserDetailsOpts,
+  SignalUnknownCredentialOpts,
+} from '../methods/sendSignal.ts';
+
 /**
- * TODO: Use `WebAuthnError` here too to try and stop the bleeding of how many `throw new Error()` are
- * introduced. This logic is specific to the signal APIs.
+ * Attempt to intuit _why_ an error was raised after calling one of the WebAuthn Signal APIs
  */
+export function identifySignalError({ error, options }: {
+  error: Error;
+  options:
+    | SignalUnknownCredentialOpts
+    | SignalAllAcceptedCredentialsOpts
+    | SignalCurrentUserDetailsOpts;
+}): WebAuthnError {
+  /**
+   * General Signal API error conditions
+   */
+  if (error.name === 'SecurityError') {
+    const effectiveDomain = globalThis.location.hostname;
+    if (!isValidDomain(effectiveDomain)) {
+      // https://w3c.github.io/webauthn/#sctn-signal-methods-async-rp-id-validation (Step 1)
+      return new WebAuthnError({
+        message: `${globalThis.location.hostname} is an invalid domain`,
+        code: 'ERROR_INVALID_DOMAIN',
+        cause: error,
+      });
+    }
+
+    // https://w3c.github.io/webauthn/#sctn-signal-methods-async-rp-id-validation (Step 3)
+    return new WebAuthnError({
+      message:
+        `The browser does not support Related Origins to enable signals for RP ID ${options.rpID} on this domain`,
+      code: 'ERROR_INVALID_RP_ID',
+      cause: error,
+    });
+  }
+
+  /**
+   * Signal-specific error conditions
+   */
+  if (options.signalName === 'signalUnknownCredential') {
+    if (error.name === 'TypeError') {
+      // https://w3c.github.io/webauthn/#sctn-signalUnknownCredential (Step 1)
+      return new WebAuthnError({
+        message: 'credentialID is an invalid base64url string',
+        code: 'ERROR_SIGNAL_INVALID_ARGUMENT',
+        cause: error,
+      });
+    }
+  } else if (options.signalName === 'signalAllAcceptedCredentials') {
+    if (error.name === 'TypeError') {
+      // https://w3c.github.io/webauthn/#sctn-signalAllAcceptedCredentials (Step 1)
+      // https://w3c.github.io/webauthn/#sctn-signalAllAcceptedCredentials (Step 2)
+      return new WebAuthnError({
+        message: 'userID, or an entry in allAcceptedCredentialIDs, is an invalid base64url string',
+        code: 'ERROR_SIGNAL_INVALID_ARGUMENT',
+        cause: error,
+      });
+    }
+  } else if (options.signalName === 'signalCurrentUserDetails') {
+    if (error.name === 'TypeError') {
+      // https://w3c.github.io/webauthn/#sctn-signalCurrentUserDetails
+      return new WebAuthnError({
+        message: 'userID is an invalid base64url string',
+        code: 'ERROR_SIGNAL_INVALID_ARGUMENT',
+        cause: error,
+      });
+    }
+  }
+
+  // Consistently return a WebAuthnError, but point to the original error for more info
+  return new WebAuthnError({
+    message: error.message,
+    code: 'ERROR_PASSTHROUGH_SEE_CAUSE_PROPERTY',
+    cause: error,
+  });
+}
