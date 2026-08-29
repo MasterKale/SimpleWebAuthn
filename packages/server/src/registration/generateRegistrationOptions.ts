873dacf1f28c45e9e41af8a36969cc04b006613d
Matthew Miller <matthew@millerti.me>
2026-08-29 11:03:29 -0700

    Get rid of duplicate list of supported algs

--- packages/server/src/registration/generateRegistrationOptions.ts
+++ packages/server/src/registration/generateRegistrationOptions.ts
@@ -2,7 +2,6 @@ import type {
   AuthenticationExtensionsClientInputs,
   AuthenticatorSelectionCriteria,
   Base64URLString,
-  COSEAlgorithmIdentifier,
   PublicKeyCredentialCreationOptionsJSON,
   PublicKeyCredentialHint,
   PublicKeyCredentialParameters,
@@ -11,42 +10,9 @@ import type {
 import { generateChallenge } from '../helpers/generateChallenge.ts';
 import { generateUserID } from '../helpers/generateUserID.ts';
 import { isoBase64URL, isoUint8Array } from '../helpers/iso/index.ts';
+import { COSEALG } from '../helpers/cose.ts';
 
 export type GenerateRegistrationOptionsOpts = Parameters<typeof generateRegistrationOptions>[0];
-
-/**
- * Supported crypto algo identifiers.
- * See https://w3c.github.io/webauthn/#sctn-alg-identifier
- * and https://www.iana.org/assignments/cose/cose.xhtml#algorithms
- */
-export const supportedCOSEAlgorithmIdentifiers: COSEAlgorithmIdentifier[] = [
-  // EdDSA (In first position to encourage authenticators to use this over ES256)
-  -8,
-  // ECDSA w/ SHA-256
-  -7,
-  // ECDSA w/ SHA-512
-  -36,
-  // RSASSA-PSS w/ SHA-256
-  -37,
-  // RSASSA-PSS w/ SHA-384
-  -38,
-  // RSASSA-PSS w/ SHA-512
-  -39,
-  // ML-DSA-44
-  -48,
-  // ML-DSA-65
-  -49,
-  // ML-DSA-87
-  -50,
-  // RSASSA-PKCS1-v1_5 w/ SHA-256
-  -257,
-  // RSASSA-PKCS1-v1_5 w/ SHA-384
-  -258,
-  // RSASSA-PKCS1-v1_5 w/ SHA-512
-  -259,
-  // RSASSA-PKCS1-v1_5 w/ SHA-1 (Deprecated; here for legacy support)
-  -65535,
-];
 
 /**
  * Set up some default authenticator selection options as per the latest spec:
@@ -66,7 +32,11 @@ const defaultAuthenticatorSelection: AuthenticatorSelectionCriteria = {
  *   - https://www.iana.org/assignments/cose/cose.xhtml#algorithms
  *   - https://w3c.github.io/webauthn/#dom-publickeycredentialcreationoptions-pubkeycredparams
  */
-const defaultSupportedAlgorithmIDs: COSEAlgorithmIdentifier[] = [-8, -7, -257];
+export const defaultSupportedAlgorithmIDs: COSEALG[] = [
+  COSEALG.EdDSA,
+  COSEALG.ES256,
+  COSEALG.RS256,
+];
 
 /**
  * Prepare a value to pass into navigator.credentials.create(...) for authenticator registration
@@ -103,7 +73,7 @@ export async function generateRegistrationOptions(
     }[];
     authenticatorSelection?: AuthenticatorSelectionCriteria;
     extensions?: AuthenticationExtensionsClientInputs;
-    supportedAlgorithmIDs?: COSEAlgorithmIdentifier[];
+    supportedAlgorithmIDs?: COSEALG[];
     preferredAuthenticatorType?: 'securityKey' | 'localDevice' | 'remoteDevice';
   },
 ): Promise<PublicKeyCredentialCreationOptionsJSON> {
