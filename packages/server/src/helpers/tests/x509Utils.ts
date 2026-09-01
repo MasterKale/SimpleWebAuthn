204e4b97080e8e63167004d2d0c9d5d02767bef4
Matthew Miller <matthew@millerti.me>
2026-08-31 22:58:29 -0700

    Create generateIntermediateCert X.509 helper

--- packages/server/src/helpers/tests/x509Utils.ts
+++ packages/server/src/helpers/tests/x509Utils.ts
@@ -51,6 +51,61 @@ export async function generateRootCert(opts: {
 }
 
 /**
+ * Generate a cert that is signed by another cert and can sign other certs to form a chain
+ */
+export async function generateIntermediateCert(
+  opts: {
+    /** Before when the cert should not be valid */
+    notBefore: Date;
+    /** After when the cert should not be valid */
+    notAfter: Date;
+    /** The certificate (and its keys) that this certificate will chain to */
+    issuer: { certificate: x509.X509Certificate; keys: CryptoKeyPair };
+    /** The optional subject for this certificate */
+    subject?: { certificate: x509.X509Certificate; keys: CryptoKeyPair };
+    /** The algorithm used for the certificate's keypair */
+    keyAlgorithm?: EcKeyGenParams;
+    /** The algorithm used for generating a signature over the certificate */
+    signingAlgorithm?: Algorithm | EcdsaParams;
+  },
+): Promise<{ certificate: x509.X509Certificate; keys: CryptoKeyPair }> {
+  const {
+    notBefore,
+    notAfter,
+    issuer,
+    subject,
+    keyAlgorithm = defaultKeyAlgorithm,
+    signingAlgorithm = defaultSigningAlgorithm,
+  } = opts;
+
+  const webCrypto = await getWebCrypto();
+
+  const certSubject = subject?.certificate.subject ??
+    'CN=SimpleWebAuthn Unit Test Intermediate Cert';
+
+  // Use the provided keys, otherwise generate a new keypair
+  const certKeys = subject?.keys ??
+    await webCrypto.subtle.generateKey(keyAlgorithm, false, ['sign', 'verify']);
+
+  const certificate = await x509.X509CertificateGenerator.create({
+    subject: certSubject,
+    notBefore,
+    notAfter,
+    issuer: issuer.certificate.subject,
+    signingKey: issuer.keys.privateKey,
+    signingAlgorithm,
+    publicKey: certKeys.publicKey,
+    extensions: [
+      // Explicitly state this cert can sign other certs
+      new x509.BasicConstraintsExtension(true, undefined, true),
+      new x509.KeyUsagesExtension(x509.KeyUsageFlags.keyCertSign, true),
+    ],
+  });
+
+  return { certificate, keys: certKeys };
+}
+
+/**
  * Generate an X.509 end-entity "leaf" certificate
  */
 export async function generateLeafCert(opts: {
